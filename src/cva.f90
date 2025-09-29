08a4754cd1f0302816c329f0ff8cfa7efbf1f5c2
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2025-09-28 21:02:48 -0400

    feat: Added something, the details of which I've forgotten.

--- src/cva.f90
+++ src/cva.f90
@@ -8,6 +8,15 @@
 module cva
 
 use prec, only: WP
+use units, only: si_length       => unit_p10_p00_p00_p00, &
+                 si_velocity     => unit_p10_p00_m10_p00, &
+                 si_mass         => unit_p00_p10_p00_p00, &
+                 si_energy       => unit_p20_p10_m20_p00, &
+                 si_area         => unit_p20_p00_p00_p00, &
+                 si_inverse_mass => unit_p00_m10_p00_p00, &
+                 si_pressure     => unit_m10_p10_m20_p00, &
+                 unitless        => unit_p00_p00_p00_p00, &
+                 si_stiffness    => unit_p00_p10_m20_p00
 implicit none
 private
 
@@ -15,7 +24,12 @@ private
 real(WP), public, parameter :: R_BAR = 8.31446261815324_WP ! J/(mol*K)
 
 ! <https://www.engineeringtoolbox.com/molecular-mass-air-d_679.html>
+! Molecular mass of air
 real(WP), public, parameter :: M_AIR = 28.9647e-3_WP ! kg/mol
+
+! moran_fundamentals_2008 table A-1
+! Critical pressure of air
+real(WP), public, parameter :: P_C_AIR = 37.7e5_WP ! Pa
 
 ! <https://en.wikipedia.org/wiki/Density_of_air>
 real(WP), public, parameter :: P_ATM   = 101325.0_WP         ! Pa
@@ -24,13 +38,30 @@ real(WP), public, parameter :: RHO_ATM = 1.2250_WP           ! kg/m3
 
 public :: p_eos
 
+type, public :: cv_type
+    ! time varying
+    type(si_length)   :: x     ! location of piston/projectile
+    type(si_velocity) :: x_dot ! velocity of piston/projectile
+    type(si_mass)     :: m     ! mass of gas in control volume
+    type(si_energy)   :: e     ! energy of gas in control volume
+    
+    ! constants
+    type(si_area)         :: csa        ! cross-sectional area
+    type(si_inverse_mass) :: rmp        ! reciprocol mass of piston/projectile
+    type(si_pressure)     :: p_fs, p_fd ! static and dynamic friction pressure
+    type(si_stiffness)    :: k          ! stiffness of spring attached to piston
+    type(si_length)       :: x_z        ! zero force location for spring
+contains
+    !procedure :: p => p_cv
+    procedure :: p_f => p_f
+end type cv_type
+
 contains
 
 pure function p_eos(rho, temp)
     use fmad, only: ad
     use units, only: si_mass_density  => unit_m30_p10_p00_p00, &
                      si_temperature   => unit_p00_p00_p00_p10, &
-                     si_pressure      => unit_m10_p10_m20_p00, &
                      si_specific_heat => unit_p20_p00_m20_m10
     use checks, only: assert
     
@@ -38,20 +69,52 @@ pure function p_eos(rho, temp)
     type(si_temperature), intent(in)  :: temp
     type(si_pressure)                 :: p_eos
     
-    integer :: n_dv
+    integer :: n_d
     
     type(si_specific_heat) :: R
     
     call assert(rho%v%v > 0.0_WP, "thermo (p_eos): rho%v > 0 violated")
     call assert(temp%v%v > 0.0_WP, "thermo (p_eos): temp%v > 0 violated")
     
-    n_dv = size(rho%v%d)
+    n_d = size(rho%v%d)
     
-    call R%v%init_const(R_BAR/M_AIR, n_dv)
+    call R%v%init_const(R_BAR/M_AIR, n_d)
     
     p_eos = rho * R * temp
     
     call assert(p_eos%v%v > 0.0_WP, "thermo (p_eos): p_eos%v > 0 violated")
+    call assert(p_eos%v%v < P_C_AIR, "thermo (p_eos): ideal gas law validity is questionable")
 end function p_eos
+
+!pure function p_cv(cv)
+!    use units, only: si_mass_density  => unit_m30_p10_p00_p00
+    
+!    class(cv_type), intent(in) :: cv
+    
+    
+!end function p_cv(cv)
+
+pure function p_f(cv, p_f0)
+    ! Returns effective pressure of friction as a function of velocity.
+    
+    use units, only: exp, abs, min, tanh, square
+    
+    class(cv_type), intent(in)    :: cv
+    type(si_pressure), intent(in) :: p_f0
+    
+    type(si_pressure) :: p_f
+    
+    integer           :: n_d
+    type(si_velocity) :: v_scale
+    type(si_pressure) :: p_scale
+    
+    n_d = size(cv%x%v%d)
+    
+    call v_scale%v%init_const(1.0_WP, n_d) ! exp underflows if I make it smaller
+    call p_scale%v%init_const(1.0_WP, n_d)
+    
+    p_f = min(cv%p_fs, abs(p_f0))*exp(-square(cv%x_dot/v_scale))*tanh(p_f0/p_scale) &
+            + cv%p_fd*tanh(cv%x_dot/v_scale)
+end function p_f
 
 end module cva
--- test/test_cva.f90
+++ test/test_cva.f90
@@ -7,26 +7,25 @@
 
 program test_cva
 
-use fmad, only: ad
-use units, only: si_mass_density => unit_m30_p10_p00_p00, &
-                 si_temperature  => unit_p00_p00_p00_p10, &
-                 si_pressure     => unit_m10_p10_m20_p00
-use prec, only: WP
-use cva, only: p_eos
 use unittest, only: test_results_type
 implicit none
 
 type(test_results_type) :: tests
 
-call tests%start_tests("thermo.nml")
+call tests%start_tests("cva.nml")
 
 call test_p_eos(tests)
+call test_p_f(tests)
 
 call tests%end_tests()
 
 contains
 
 subroutine test_p_eos(tests)
+    use units, only: si_mass_density => unit_m30_p10_p00_p00, &
+                     si_temperature  => unit_p00_p00_p00_p10, &
+                     si_pressure     => unit_m10_p10_m20_p00
+    use prec, only: WP
     use cva, only: P_ATM, T_ATM, RHO_ATM, p_eos
     
     type(test_results_type), intent(in out) :: tests
@@ -42,5 +41,24 @@ subroutine test_p_eos(tests)
     
     call tests%real_eq(p%v%v, P_ATM, "p_eos, atmospheric", abs_tol=1.0_WP)
 end subroutine test_p_eos
+
+subroutine test_p_f(tests)
+    use units, only: si_pressure => unit_m10_p10_m20_p00
+    use cva, only: cv_type
+    
+    type(test_results_type), intent(in out) :: tests
+
+    type(cv_type)     :: cv
+    type(si_pressure) :: p_f0, p_f
+    
+    call cv%x%v%init_const(0.0_WP, 0)
+    call cv%p_fs%v%init_const(0.1e5_WP, 0)
+    call cv%p_fd%v%init_const(0.05e5_WP, 0)
+    
+    call cv%x_dot%v%init_const(-10.0_WP, 0)
+    call p_f0%v%init_const(-1.0e5_WP, 0)
+    p_f = cv%p_f(p_f0)
+    call tests%real_eq(p_f%v%v, cv%p_fs, "p_f, x_dot < 0, p_f0 < 0")
+end subroutine test_p_f
 
 end program test_cva
