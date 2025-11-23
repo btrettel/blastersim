0f36e3c9a43e3d54ecfa76cea9c588e69c231720
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2025-11-22 19:46:07 -0500

    refactor: Made create_barrel use genunits types.

--- src/io.f90
+++ src/io.f90
@@ -12,52 +12,41 @@ use units
 use checks, only: assert
 implicit none
 
+public :: create_barrel
 !public :: read_springer_namelist
 
 contains
 
-subroutine create_barrel(vol_d, d, p_atm, temp_atm, m_p, p_fs, p_fd, l, n_d, cv)
+subroutine create_barrel(vol_dead, d_barrel, p_atm, temp_atm, m_p, p_fs, p_fd, l_travel, n_d, cv)
     use prec, only: PI
     use gasdata, only: DRY_AIR
     use cva, only: cv_type
     
-    real(WP), intent(in)       :: vol_d      ! dead volume (m3)
-    real(WP), intent(in)       :: d          ! barrel diameter (m)
-    real(WP), intent(in)       :: p_atm      ! atmospheric pressure (Pa), temp_atm, m_p, p_fs, p_fd, l
-    real(WP), intent(in)       :: temp_atm   ! atmospheric temperature (K)
-    real(WP), intent(in)       :: m_p        ! projectile mass (kg)p_fs, p_fd, l
-    real(WP), intent(in)       :: p_fs, p_fd ! pressures of static and dynamic friction, respectively (Pa)
-    real(WP), intent(in)       :: l          ! barrel length (projectile travel) (m)
-    integer, intent(in)        :: n_d
-    type(cv_type), intent(out) :: cv
+    type(si_volume), intent(in)      :: vol_dead   ! dead volume
+    type(si_length), intent(in)      :: d_barrel   ! barrel diameter
+    type(si_pressure), intent(in)    :: p_atm      ! atmospheric pressure
+    type(si_temperature), intent(in) :: temp_atm   ! atmospheric temperature
+    type(si_mass), intent(in)        :: m_p        ! projectile mass
+    type(si_pressure)                :: p_fs, p_fd ! pressures of static and dynamic friction, respectively
+    type(si_length), intent(in)      :: l_travel   ! projectile travel
+    integer, intent(in)              :: n_d
+    type(cv_type), intent(out)       :: cv
     
-    type(si_volume)      :: vol_d_
-    type(si_length)      :: d_, x_d, l_, x_stop, x_z
-    type(si_area)        :: csa
-    type(si_velocity)    :: x_dot
-    type(unitless)       :: y(1)
-    type(si_pressure)    :: p_atm_, p_fs_, p_fd_
-    type(si_temperature) :: temp_atm_
-    type(si_mass)        :: m_p_
-    type(si_stiffness)   :: k
+    type(si_length)    :: x_d, x_stop, x_z
+    type(si_area)      :: csa_barrel
+    type(si_velocity)  :: x_dot
+    type(unitless)     :: y(1)
+    type(si_stiffness) :: k
     
-    call vol_d_%v%init_const(vol_d, n_d)
-    call d_%v%init_const(d, n_d)
-    csa = (PI/4.0_WP)*square(d_)
-    x_d = vol_d_/csa
     call x_dot%v%init_const(0.0_WP, n_d)
     call y(1)%v%init_const(1.0_WP, n_d)
-    call p_atm_%v%init_const(p_atm, n_d)
-    call temp_atm_%v%init_const(temp_atm, n_d)
-    call m_p_%v%init_const(m_p, n_d)
-    call p_fs_%v%init_const(p_fs, n_d)
-    call p_fd_%v%init_const(p_fd, n_d)
-    call l_%v%init_const(l, n_d)
-    x_stop = x_d + l_
+    csa_barrel = (PI/4.0_WP)*square(d_barrel)
+    x_d = vol_dead/csa_barrel
     call k%v%init_const(0.0_WP, n_d)
     call x_z%v%init_const(0.0_WP, n_d)
+    x_stop = x_d + l_travel
     
-    call cv%set(x_d, x_dot, y, p_atm_, temp_atm_, csa, 1.0_WP/m_p_, p_fs_, p_fd_, p_atm_, k, x_z, [DRY_AIR], x_stop)
+    call cv%set(x_d, x_dot, y, p_atm, temp_atm, csa_barrel, 1.0_WP/m_p, p_fs, p_fd, p_atm, k, x_z, [DRY_AIR], x_stop)
 end subroutine create_barrel
 
 !subroutine read_springer_namelist(input_file, sys, rc)
