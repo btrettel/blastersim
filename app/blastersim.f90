btrettel/blastersim

commit 4e72fc49c29ff6b1db1c9b496701e7fa9c25033f

refactor: Changes to support fuzz testing via nmlfuzz.

app/blastersim.f90
@@ -73,12 +73,12 @@ else
         case (TIMEOUT_RUN_RC)
             write(unit=ERROR_UNIT, fmt="(a, f3.1, a)") "Projectile did not leave barrel after ", &
                                                             T_STOP_DEFAULT, " seconds."
+            stop EX_USAGE, quiet=.true.
         case default
             write(unit=ERROR_UNIT, fmt="(a)") "Refer to BlasterSim User's Guide for meaning of return code."
             write(unit=ERROR_UNIT, fmt="(a)") "<http://trettel.us/blastersim/docs/verification.html#run-time-checks>"
+            stop EX_SOFTWARE, quiet=.true.
     end select
-    
-    stop EX_USAGE, quiet=.true.
 end if
 !tripwire$ end
 

src/geninput_pneumatic.nml
@@ -1,9 +1,11 @@
 &geninput_config
-output_file_prefix = "src/geninput_pneumatic"
-namelist_group     = "pneumatic"
-kind_parameter     = "WP"
-write_tex          = .true.
-write_return       = .false.
+output_file_prefix    = "src/geninput_pneumatic"
+namelist_group        = "pneumatic"
+kind_parameter        = "WP"
+write_tex             = .true.
+write_return          = .false.
+executable            = "./blastersim"
+acceptable_exit_codes = 0, 64
 /
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -29,6 +31,16 @@ bound_fmt             = "f4.2"
 /
 
 &input_variable
+variable_name         = "csv_output"
+type_definition       = "logical"
+required              = .false.
+tex_description       = "Whether CSV output is enabled or disabled."
+default_value         = ".true."
+fuzz_range            = 0.0, 0.0
+fuzz                  = .true. ! needed to fuzz with a range of 0
+/
+
+&input_variable
 variable_name         = "d_barrel"
 type_definition       = "type(si_length)"
 required              = .true.
@@ -41,6 +53,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-3, 20.0e-3
 /
 
 &input_variable
@@ -56,6 +69,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-2, 200.0e-2
 /
 
 &input_variable
@@ -71,6 +85,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-6, 100.0e-6
 /
 
 &input_variable
@@ -117,6 +132,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-3, 50.0e-3
 /
 
 &input_variable
@@ -149,6 +165,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .false.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 10000.0
 /
 
 &input_variable
@@ -164,6 +181,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .false.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 10000.0
 /
 
 &input_variable
@@ -179,6 +197,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.25, 500.0
 /
 
 &input_variable
@@ -296,6 +315,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-2, 20.0e-2
 /
 
 &input_variable
@@ -311,6 +331,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-6, 1000.0e-6
 /
 
 &input_variable
@@ -326,4 +347,5 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 100.0, 500.0
 /

src/geninput_springer.nml
@@ -1,9 +1,11 @@
 &geninput_config
-output_file_prefix = "src/geninput_springer"
-namelist_group     = "springer"
-kind_parameter     = "WP"
-write_tex          = .true.
-write_return       = .false.
+output_file_prefix    = "src/geninput_springer"
+namelist_group        = "springer"
+kind_parameter        = "WP"
+write_tex             = .true.
+write_return          = .false.
+executable            = "./blastersim"
+acceptable_exit_codes = 0, 64
 /
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -29,6 +31,16 @@ bound_fmt             = "f4.2"
 /
 
 &input_variable
+variable_name         = "csv_output"
+type_definition       = "logical"
+required              = .false.
+tex_description       = "Whether CSV output is enabled or disabled."
+default_value         = ".true."
+fuzz_range            = 0.0, 0.0
+fuzz                  = .true. ! needed to fuzz with a range of 0
+/
+
+&input_variable
 variable_name         = "d_barrel"
 type_definition       = "type(si_length)"
 required              = .true.
@@ -41,6 +53,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-3, 20.0e-3
 /
 
 &input_variable
@@ -56,6 +69,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-2, 200.0e-2
 /
 
 &input_variable
@@ -71,6 +85,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-6, 100.0e-6
 /
 
 &input_variable
@@ -118,6 +133,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-3, 20.0e-3
 /
 
 &input_variable
@@ -150,6 +166,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .false.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 10000.0
 /
 
 &input_variable
@@ -165,6 +182,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .false.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 10000.0
 /
 
 &input_variable
@@ -180,6 +198,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.25, 10.0
 /
 
 &input_variable
@@ -282,6 +301,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-3, 50.0e-3
 /
 
 &input_variable
@@ -311,6 +331,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-3, 500.0e-3
 /
 
 &input_variable
@@ -325,6 +346,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0e-2, 50.0e-2
 /
 
 &input_variable
@@ -340,6 +362,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .false.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 10.0e-2
 /
 
 &input_variable
@@ -354,6 +377,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 100.0, 2000.0
 /
 
 &input_variable
@@ -369,6 +393,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .true.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 1.0, 500.0
 /
 
 &input_variable
@@ -384,6 +409,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .false.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 10000.0
 /
 
 &input_variable
@@ -399,6 +425,7 @@ lower_bound           = 0.0
 lower_bound_not_equal = .false.
 default_value         = "-1.0"
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 10000.0
 /
 
 &input_variable
@@ -418,4 +445,5 @@ upper_bound_not_equal = .false.
 default_value         = "COR_DEFAULT"
 no_kind_default_value = .true.
 bound_fmt             = "f3.1"
+fuzz_range            = 0.0, 1.0
 /

src/io.f90
@@ -164,7 +164,7 @@ subroutine read_pneumatic_namelist(input_file, sys, config, rc_read, actual_v_mu
     ! `sys%cv(I_BARREL_ATM)`: atmosphere
     call sys%cv(I_BARREL_ATM)%set_const("atmosphere", csa_barrel, p_atm_u, temp_atm_u, ATM_GAS, y, I_BARREL)
     
-    call config%set(id, csv_output=.true., dt=dt_u, n_d=0)
+    call config%set(id, csv_output=csv_output, dt=dt_u, n_d=0)
     
     if (present(actual_v_muzzle_))       actual_v_muzzle_       = actual_v_muzzle_u
     if (present(actual_v_muzzle_stdev_)) actual_v_muzzle_stdev_ = actual_v_muzzle_stdev_u
@@ -271,7 +271,7 @@ subroutine read_springer_namelist(input_file, sys, config, rc_read, actual_v_muz
     call sys%cv(I_PLUNGER_ATM)%set_const("atmosphere for plunger", csa_plunger, p_atm_u, temp_atm_u, PLUNGER_ATM_GAS, &
                                             y, I_PLUNGER)
     
-    call config%set(id, csv_output=.true., dt=dt_u, n_d=0)
+    call config%set(id, csv_output=csv_output, dt=dt_u, n_d=0)
     
     if (present(actual_v_muzzle_))       actual_v_muzzle_       = actual_v_muzzle_u
     if (present(actual_v_muzzle_stdev_)) actual_v_muzzle_stdev_ = actual_v_muzzle_stdev_u

test/pneumatic-non-default.nml
@@ -1,5 +1,6 @@
 &pneumatic
 dt = 2.0e-7 ! s
+csv_output = .false.
 
 ! atmosphere
 p_atm    = 102.0 ! kPa (absolute)

test/springer-non-default.nml
@@ -1,5 +1,6 @@
 &springer
 dt = 1.0e-7 ! s
+csv_output = .false.
 
 ! barrel
 d_barrel  = 13.0e-3 ! m

test/test_cva.f90
@@ -1623,8 +1623,8 @@ subroutine test_conservation_1(tests)
     
     call sys_start%cv(1)%set_const("atmosphere for chamber", csa_3, p_atm, temp_atm, [DRY_AIR], y, 3, x_dot=-x_dot)
     
-    call tests%integer_eq(sys_start%cv(1)%type, MIRROR_CV_TYPE, "test_conservation, sys_start%cv(1)%type")
-    call tests%integer_eq(sys_start%cv(1)%i_cv_mirror, 3, "test_conservation, sys_start%cv(1)%i_cv_mirror")
+    call tests%integer_eq(sys_start%cv(1)%type, MIRROR_CV_TYPE, "test_conservation_1, sys_start%cv(1)%type")
+    call tests%integer_eq(sys_start%cv(1)%i_cv_mirror, 3, "test_conservation_1, sys_start%cv(1)%i_cv_mirror")
     
     ! 2: atmosphere for barrel
     call p_atm%v%init_const(1.0e5_WP, n_d)
@@ -1633,8 +1633,8 @@ subroutine test_conservation_1(tests)
     
     call sys_start%cv(2)%set_const("atmosphere for barrel", csa_4, p_atm, temp_atm, [DRY_AIR], y, 4)
     
-    call tests%integer_eq(sys_start%cv(2)%type, MIRROR_CV_TYPE, "test_conservation, sys_start%cv(2)%type")
-    call tests%integer_eq(sys_start%cv(2)%i_cv_mirror, 4, "test_conservation, sys_start%cv(2)%i_cv_mirror")
+    call tests%integer_eq(sys_start%cv(2)%type, MIRROR_CV_TYPE, "test_conservation_1, sys_start%cv(2)%type")
+    call tests%integer_eq(sys_start%cv(2)%i_cv_mirror, 4, "test_conservation_1, sys_start%cv(2)%i_cv_mirror")
     
     ! 3: chamber
     call x_3%v%init_const(10.0e-2_WP, n_d)
@@ -1667,7 +1667,7 @@ subroutine test_conservation_1(tests)
     call config%set("test_conservation", 1, csv_output=.true., csv_frequency=100)
     call run(config, sys_start, sys_end, status)
     
-    call tests%integer_eq(status%rc, X_GE_X_STOP_RUN_RC, "test_conservation, status%rc")
+    call tests%integer_eq(status%rc, X_GE_X_STOP_RUN_RC, "test_conservation_1, status%rc")
     
     if (status%rc /= X_GE_X_STOP_RUN_RC) then
         if (allocated(status%data)) print *, status%data(1)
@@ -1675,49 +1675,49 @@ subroutine test_conservation_1(tests)
     end if
     
     e_s_3_start = sys_start%cv(3)%e_s()
-    call tests%real_eq(e_s_3_start%v%v, 0.5_WP*(700.0_WP)*(9.0e-2_WP)**2, "test_conservation, e_s_3_start")
+    call tests%real_eq(e_s_3_start%v%v, 0.5_WP*(700.0_WP)*(9.0e-2_WP)**2, "test_conservation_1, e_s_3_start")
     
     e_k_3_start = sys_start%cv(3)%e_k()
-    call tests%real_eq(e_k_3_start%v%v, 0.5_WP*(30.0e-3_WP)*(2.0_WP)**2, "test_conservation, e_k_3_start")
+    call tests%real_eq(e_k_3_start%v%v, 0.5_WP*(30.0e-3_WP)*(2.0_WP)**2, "test_conservation_1, e_k_3_start")
     
     e_start_3 = sys_start%cv(3)%e_total()
     call tests%real_eq(e_start_3%v%v, sys_start%cv(3)%e_g%v%v + e_s_3_start%v%v + e_k_3_start%v%v, &
-                            "test_conservation, sys_start%cv(3)%e_total()")
+                            "test_conservation_1, sys_start%cv(3)%e_total()")
     
     m_start = sys_start%m_total()
     m_end   = sys_end%m_total()
-    call tests%real_eq(m_start%v%v, m_end%v%v, "test_conservation, m_start == m_end")
-    call tests%real_gt(m_start%v%d(1), 0.0_WP, "test_conservation, m_start derivative is positive")
-    call tests%real_eq(m_start%v%d(1) - m_end%v%d(1), 0.0_WP, "test_conservation, delta m derivative is zero", abs_tol=1.0e-22_WP)
+    call tests%real_eq(m_start%v%v, m_end%v%v, "test_conservation_1, m_start == m_end")
+    call tests%real_gt(m_start%v%d(1), 0.0_WP, "test_conservation_1, m_start derivative is positive")
+    call tests%real_eq(m_start%v%d(1) - m_end%v%d(1), 0.0_WP, "test_conservation_1, delta m derivative is zero", abs_tol=1.0e-22_WP)
     
     e_start = sys_start%e_total()
     e_end   = sys_end%e_total()
-    call tests%real_eq(e_start%v%v, e_end%v%v, "test_conservation, e_start == e_end", abs_tol=1.0e-3_WP)
-    call tests%real_gt(e_start%v%d(1), 0.0_WP, "test_conservation, e_start derivative is positive")
-    call tests%real_eq(e_start%v%d(1) - e_end%v%d(1), 0.0_WP, "test_conservation, delta e derivative is zero", abs_tol=1.0e-5_WP)
+    call tests%real_eq(e_start%v%v, e_end%v%v, "test_conservation_1, e_start == e_end", abs_tol=1.0e-3_WP)
+    call tests%real_gt(e_start%v%d(1), 0.0_WP, "test_conservation_1, e_start derivative is positive")
+    call tests%real_eq(e_start%v%d(1) - e_end%v%d(1), 0.0_WP, "test_conservation_1, delta e derivative is zero", abs_tol=1.0e-5_WP)
     
     e_chamber_atm = sys_end%cv(1)%e_total()
-    call tests%character_eq(sys_end%cv(1)%label, "atmosphere for chamber", "test_conservation, chamber atmosphere label")
-    call tests%real_lt(e_chamber_atm%v%v, 0.0_WP, "test_conservation, chamber atmosphere energy sign")
+    call tests%character_eq(sys_end%cv(1)%label, "atmosphere for chamber", "test_conservation_1, chamber atmosphere label")
+    call tests%real_lt(e_chamber_atm%v%v, 0.0_WP, "test_conservation_1, chamber atmosphere energy sign")
     call tests%real_gt(abs(sys_end%cv(1)%x_dot%v%v), 0.0_WP, &
-                        "test_conservation, chamber atmosphere plunger velocity is non-zero")
+                        "test_conservation_1, chamber atmosphere plunger velocity is non-zero")
     call tests%real_eq(sys_end%cv(1)%x_dot%v%v, -sys_end%cv(3)%x_dot%v%v, &
-                        "test_conservation, chamber atmosphere velocity matches plunger velocity")
+                        "test_conservation_1, chamber atmosphere velocity matches plunger velocity")
     call tests%real_eq(e_chamber_atm%v%v, sys_end%cv(1)%e_g%v%v, &
-                            "test_conservation, chamber atmosphere energy is internal energy")
+                            "test_conservation_1, chamber atmosphere energy is internal energy")
     
     e_barrel_atm = sys_end%cv(2)%e_total()
-    call tests%character_eq(sys_end%cv(2)%label, "atmosphere for barrel", "test_conservation, barrel atmosphere label")
-    call tests%real_gt(e_barrel_atm%v%v, 0.0_WP, "test_conservation, barrel atmosphere energy sign")
+    call tests%character_eq(sys_end%cv(2)%label, "atmosphere for barrel", "test_conservation_1, barrel atmosphere label")
+    call tests%real_gt(e_barrel_atm%v%v, 0.0_WP, "test_conservation_1, barrel atmosphere energy sign")
     call tests%real_gt(abs(sys_end%cv(2)%x_dot%v%v), 0.0_WP, &
-                        "test_conservation, barrel atmosphere projectile/plunger velocity is non-zero")
+                        "test_conservation_1, barrel atmosphere projectile/plunger velocity is non-zero")
     call tests%real_eq(sys_end%cv(2)%x_dot%v%v, -sys_end%cv(4)%x_dot%v%v, &
-                        "test_conservation, barrel atmosphere velocity matches projectile velocity")
+                        "test_conservation_1, barrel atmosphere velocity matches projectile velocity")
     call tests%real_eq(e_barrel_atm%v%v, sys_end%cv(2)%e_g%v%v, &
-                            "test_conservation, barrel atmosphere energy is internal energy")
+                            "test_conservation_1, barrel atmosphere energy is internal energy")
     
-    call tests%real_gt(sys_end%cv(3)%e_f%v%v, 0.0_WP, "test_conservation, chamber friction loss is non-zero")
-    call tests%real_gt(sys_end%cv(4)%e_f%v%v, 0.0_WP, "test_conservation, barrel friction loss is non-zero")
+    call tests%real_gt(sys_end%cv(3)%e_f%v%v, 0.0_WP, "test_conservation_1, chamber friction loss is non-zero")
+    call tests%real_gt(sys_end%cv(4)%e_f%v%v, 0.0_WP, "test_conservation_1, barrel friction loss is non-zero")
 end subroutine test_conservation_1
 
 subroutine test_conservation_2(tests)

test/test_io.f90
@@ -58,6 +58,9 @@ subroutine test_read_pneumatic_namelist_non_default(tests)
     call tests%integer_eq(rc, 0, "read_pneumatic_namelist, non-default values, rc")
     
     if (rc == 0) then
+        call tests%real_eq(config%dt%v%v, 2.0e-7_WP, "read_pneumatic_namelist, non-default values, dt")
+        call tests%logical_false(config%csv_output, "read_pneumatic_namelist, non-default values, csv_output")
+        
         d_barrel = sqrt((4.0_WP/PI)*sys%cv(I_BARREL)%csa)
         call tests%real_eq(d_barrel%v%v, 13.0e-3_WP, "read_pneumatic_namelist, non-default values, d_barrel")
         
@@ -79,7 +82,6 @@ subroutine test_read_pneumatic_namelist_non_default(tests)
         
         call tests%character_eq(config%id, "pneumatic-non-default", "read_pneumatic_namelist, non-default values, id")
         
-        call tests%real_eq(config%dt%v%v, 2.0e-7_WP, "read_pneumatic_namelist, non-default values, dt")
         call tests%real_eq(sys%con(I_CHAMBER, I_BARREL)%b%v%v, 0.6_WP, "read_pneumatic_namelist, non-default values, b")
         call tests%real_eq(sys%cv(I_BARREL_ATM)%p_const%v%v, 102000.0_WP, "read_pneumatic_namelist, non-default values, p_atm")
         call tests%real_eq(sys%cv(I_BARREL_ATM)%temp_const%v%v, 295.0_WP, &
@@ -119,6 +121,9 @@ subroutine test_read_pneumatic_namelist_default(tests)
     call tests%integer_eq(rc, 0, "read_pneumatic_namelist, default values, rc")
     
     if (rc == 0) then
+        call tests%real_eq(config%dt%v%v, DT_DEFAULT, "read_pneumatic_namelist, default values, dt")
+        call tests%logical_true(config%csv_output, "read_pneumatic_namelist, default values, csv_output")
+        
         d_barrel = sqrt((4.0_WP/PI)*sys%cv(I_BARREL)%csa)
         call tests%real_eq(d_barrel%v%v, 13.0e-3_WP, "read_pneumatic_namelist, default values, d_barrel")
         
@@ -140,7 +145,6 @@ subroutine test_read_pneumatic_namelist_default(tests)
         
         call tests%character_eq(config%id, "pneumatic-default", "read_pneumatic_namelist, default values, id")
         
-        call tests%real_eq(config%dt%v%v, DT_DEFAULT, "read_pneumatic_namelist, default values, dt")
         call tests%real_eq(sys%con(I_CHAMBER, I_BARREL)%b%v%v, 0.5_WP, "read_pneumatic_namelist, default values, b")
         call tests%real_eq(sys%cv(I_BARREL_ATM)%p_const%v%v, P_ATM, "read_pneumatic_namelist, default values, p_atm")
         call tests%real_eq(sys%cv(I_BARREL_ATM)%temp_const%v%v, TEMP_ATM, "read_pneumatic_namelist, default values, temp_atm")
@@ -178,6 +182,9 @@ subroutine test_read_springer_namelist_non_default(tests)
     call tests%integer_eq(rc, 0, "read_springer_namelist, non-default values, rc")
     
     if (rc == 0) then
+        call tests%real_eq(config%dt%v%v, 1.0e-7_WP, "read_springer_namelist, non-default values, dt")
+        call tests%logical_false(config%csv_output, "read_springer_namelist, non-default values, csv_output")
+        
         d_barrel = sqrt((4.0_WP/PI)*sys%cv(I_BARREL)%csa)
         call tests%real_eq(d_barrel%v%v, 13.0e-3_WP, "read_springer_namelist, non-default values, d_barrel")
         
@@ -213,7 +220,6 @@ subroutine test_read_springer_namelist_non_default(tests)
         
         call tests%character_eq(config%id, "springer-non-default", "read_springer_namelist, non-default values, id")
         
-        call tests%real_eq(config%dt%v%v, 1.0e-7_WP, "read_springer_namelist, non-default values, dt")
         call tests%real_eq(sys%con(I_PLUNGER, I_BARREL)%b%v%v, 0.528_WP, "read_springer_namelist, non-default values, b")
         call tests%real_eq(sys%cv(I_PLUNGER_ATM)%p_const%v%v, 100000.0_WP, &
                                     "read_springer_namelist, non-default values, p_atm (1)")
@@ -258,6 +264,9 @@ subroutine test_read_springer_namelist_default(tests)
     call tests%integer_eq(rc, 0, "read_springer_namelist, default values, rc")
     
     if (rc == 0) then
+        call tests%real_eq(config%dt%v%v, DT_DEFAULT, "read_springer_namelist, default values, dt")
+        call tests%logical_true(config%csv_output, "read_springer_namelist, default values, csv_output")
+        
         d_barrel = sqrt((4.0_WP/PI)*sys%cv(I_BARREL)%csa)
         call tests%real_eq(d_barrel%v%v, 13.0e-3_WP, "read_springer_namelist, default values, d_barrel")
         
@@ -292,7 +301,6 @@ subroutine test_read_springer_namelist_default(tests)
         
         call tests%character_eq(config%id, "springer-default", "read_springer_namelist, default values, id")
         
-        call tests%real_eq(config%dt%v%v, DT_DEFAULT, "read_springer_namelist, default values, dt")
         call tests%real_eq(sys%con(I_PLUNGER, I_BARREL)%b%v%v, 0.5_WP, "read_springer_namelist, default values, b")
         call tests%real_eq(sys%cv(I_PLUNGER_ATM)%p_const%v%v, P_ATM, "read_springer_namelist, default values, p_atm (1)")
         call tests%real_eq(sys%cv(I_BARREL_ATM)%p_const%v%v, P_ATM, "read_springer_namelist, default values, p_atm (2)")

commit 03cd98973ed5af3b99957022145b73ef8838c442

fix: Fixed a tripwire checksum. Fixes: 4e72fc49c29ff6b1db1c9b496701e7fa9c25033f ("refactor: Changes to support fuzz testing via nmlfuzz.")

app/blastersim.f90
@@ -61,7 +61,7 @@ end block nml_blk
 
 call run(config, sys_start, sys_end, status)
 
-!tripwire$ begin 56F2042F Update `\secref{run-time-checks}` of verval.tex.
+!tripwire$ begin 1D473D36 Update `\secref{run-time-checks}` of verval.tex.
 if (status%rc < SUCCESS_RC) then
     write(unit=OUTPUT_UNIT, fmt="(a)") "SUCCESS!"
     write(unit=OUTPUT_UNIT, fmt="(a, f0.2, a)") "muzzle velocity: ", sys_end%cv(I_BARREL)%x_dot%v%v, " m/s"