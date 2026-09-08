btrettel/blastersim

commit b1441af060fd512d8ef32988f12acc30fba8ec0e

fix: Made effective inverse mass zero for stationary x control volumes. Fixed the name of a variable.

src/cva.f90
@@ -128,7 +128,7 @@ contains
     procedure :: u        => u_cv
     procedure :: h        => h_cv
     procedure :: gamma    => gamma_cv
-    procedure :: r_mp_eff => r_mp_eff_cv
+    procedure :: rm_p_eff => rm_p_eff_cv
     procedure :: set
     procedure :: set_const
     procedure :: p_f
@@ -226,7 +226,7 @@ pure function e_k(cv)
     class(cv_type), intent(in) :: cv
     
     type(si_energy)       :: e_k
-    type(si_inverse_mass) :: r_mp_eff ! effective mass of projectile/plunger
+    type(si_inverse_mass) :: rm_p_eff ! effective mass of projectile/plunger
     
     if (is_close(cv%rm_p%v%v, 0.0_WP) .or. (cv%type == MIRROR_CV_TYPE)) then
         if (is_close(cv%rm_p%v%v, 0.0_WP)) then
@@ -237,8 +237,8 @@ pure function e_k(cv)
         
         call e_k%v%init_const(0.0_WP, size(cv%x_dot%v%d))
     else
-        r_mp_eff = cv%rm_p / (1.0_WP + C_MS * cv%m_spring * cv%rm_p)
-        e_k = 0.5_WP*square(cv%x_dot)/r_mp_eff
+        rm_p_eff = cv%rm_p_eff()
+        e_k = 0.5_WP*square(cv%x_dot)/rm_p_eff
     end if
 end function e_k
 
@@ -1073,7 +1073,7 @@ pure function d_x_dot_d_t_normal(sys, i_cv)
     
     type(si_pressure)     :: p_fe ! friction pressure at equilibrium ($\partial \dot{x}/\partial t = 0$)
     type(si_pressure)     :: p_mirror
-    type(si_inverse_mass) :: r_mp_eff ! effective inverse mass of projectile/plunger
+    type(si_inverse_mass) :: rm_p_eff ! effective inverse mass of projectile/plunger
     
     call assert(sys%cv(i_cv)%csa%v%v > 0.0_WP, "cva (d_x_dot_d_t_normal): cv%csa > 0 violated", &
                     print_real=[sys%cv(i_cv)%csa%v%v], print_integer=[i_cv])
@@ -1094,22 +1094,22 @@ pure function d_x_dot_d_t_normal(sys, i_cv)
     p_fe = sys%cv(i_cv)%p() - p_mirror &
                 - (sys%cv(i_cv)%k/sys%cv(i_cv)%csa)*(sys%cv(i_cv)%x - sys%cv(i_cv)%x_min + sys%cv(i_cv)%delta_pre)
     
-    r_mp_eff = sys%cv(i_cv)%r_mp_eff()
+    rm_p_eff = sys%cv(i_cv)%rm_p_eff()
     
-    d_x_dot_d_t_normal = sys%cv(i_cv)%csa*r_mp_eff*(sys%cv(i_cv)%p() - p_mirror - sys%cv(i_cv)%p_f(p_fe)) &
-                            - sys%cv(i_cv)%k*r_mp_eff*(sys%cv(i_cv)%x + sys%cv(i_cv)%delta_pre)
+    d_x_dot_d_t_normal = sys%cv(i_cv)%csa*rm_p_eff*(sys%cv(i_cv)%p() - p_mirror - sys%cv(i_cv)%p_f(p_fe)) &
+                            - sys%cv(i_cv)%k*rm_p_eff*(sys%cv(i_cv)%x + sys%cv(i_cv)%delta_pre)
 end function d_x_dot_d_t_normal
 
-pure function r_mp_eff_cv(cv)
+pure function rm_p_eff_cv(cv)
     ! This calculates the effective inverse mass of the projectile/plunger factoring in the spring mass.
     ! ruby_equivalent_2000
     
     class(cv_type), intent(in) :: cv
     
-    type(si_inverse_mass) :: r_mp_eff_cv
+    type(si_inverse_mass) :: rm_p_eff_cv
     
-    r_mp_eff_cv = cv%rm_p / (1.0_WP + C_MS * cv%m_spring * cv%rm_p)
-end function r_mp_eff_cv
+    rm_p_eff_cv = cv%rm_p / (1.0_WP + C_MS * cv%m_spring * cv%rm_p)
+end function rm_p_eff_cv
 !tripwire$ end
 
 !tripwire$ begin B862672E Update \secref{conservation-laws} of theory.tex if necessary.
@@ -1224,6 +1224,10 @@ pure function d_e_f_d_t(sys, i_cv)
             p_fe = sys%cv(i_cv)%p() - p_mirror - (sys%cv(i_cv)%k/sys%cv(i_cv)%csa)*(sys%cv(i_cv)%x + sys%cv(i_cv)%delta_pre)
             
             d_e_f_d_t = sys%cv(i_cv)%p_f(p_fe) * sys%cv(i_cv)%csa * sys%cv(i_cv)%x_dot
+            
+            ! TODO: Reenable this assertion after figuring out why `e_f` decreases in some cases.
+!            call assert(d_e_f_d_t%v%v >= 0.0_WP, "cva (d_e_f_d_t): e_f should not decrease", &
+!                            print_real=[d_e_f_d_t%v%v])
         case (MIRROR_CV_TYPE)
             call d_e_f_d_t%v%init_const(0.0_WP, size(sys%cv(i_cv)%csa%v%d))
             call assert(is_close(sys%cv(i_cv)%e_f%v%v, 0.0_WP), "cva (d_e_f_d_t): e_f must be zero for MIRROR_CV_TYPE", &
@@ -1682,12 +1686,8 @@ subroutine run(config, sys_start, sys_end, status)
         if (status%rc == X_LT_X_MIN_RUN_RC) then
             call get_sys_at_x(t_old, dt, status%i_cv(1), sys_old%cv(status%i_cv(1))%x_min, &
                                 sys_old, sys_new, t, sys_event, rc_get_sys_at_x)
-            !print *, "impact 1", t_old%v%v, t%v%v, rc_get_sys_at_x
             if (rc_get_sys_at_x == SUCCESS_RC) then
                 call get_sys_after_impact(status%i_cv(1), sys_event, sys_new)
-!                print *, "impact 2", sys_old%cv(status%i_cv(1))%x_dot%v%v, &
-!                                        sys_event%cv(status%i_cv(1))%x_dot%v%v, &
-!                                        sys_new%cv(status%i_cv(1))%x_dot%v%v
             else
                 status%rc = MAX_ITER_GET_SYS_AT_X_RUN_RC
             end if
@@ -2060,9 +2060,13 @@ pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact)
     
     sys_after_impact                = sys_before_impact
     sys_after_impact%cv(i_cv)%x_dot = -sys_before_impact%cv(i_cv)%cor*sys_before_impact%cv(i_cv)%x_dot
-    sys_after_impact%cv(i_cv)%e_m   = sys_before_impact%cv(i_cv)%e_m &
-                                        + (0.5_WP/sys_after_impact%cv(i_cv)%r_mp_eff()) &
-                                            *(square(sys_before_impact%cv(i_cv)%x_dot) - square(sys_after_impact%cv(i_cv)%x_dot))
+    
+    if (.not. is_close(sys_after_impact%cv(i_cv)%rm_p%v%v, 0.0_WP)) then
+        sys_after_impact%cv(i_cv)%e_m = sys_before_impact%cv(i_cv)%e_m &
+                                            + (0.5_WP/sys_after_impact%cv(i_cv)%rm_p_eff()) &
+                                                *(square(sys_before_impact%cv(i_cv)%x_dot) &
+                                                    - square(sys_after_impact%cv(i_cv)%x_dot))
+    end if
 end subroutine get_sys_after_impact
 !tripwire$ end
 

docs/blastersim.bib
@@ -13,7 +13,19 @@
 % ? unused Language ("en")
 % ? unused Library catalog ("DOI.org (Crossref)")
 % ? unused Number of pages ("338")
-% ? unused Url ("http://link.springer.com/10.1007/978-3-540-69471-7")
+% ? unused URL ("http://link.springer.com/10.1007/978-3-540-69471-7")
+
+@book{beer_vector_1962-1,
+  title = {Vector Mechanics for Engineers: Dynamics},
+  author = {Beer, Ferdinand P. and Johnston, E. Russell},
+  year = 1962,
+  volume = {2},
+  publisher = {McGraw-Hill},
+  address = {New York}
+}
+% == BibTeX quality report for beer_vector_1962-1:
+% ? unused Call number ("QA807 .B394")
+% ? unused Library catalog ("Library Catalog (EBSCO Locate)")
 
 @book{corner_theory_1950,
   title = {Theory of the Interior Ballistics of Guns},
@@ -40,7 +52,7 @@
 }
 % == BibTeX quality report for deb_efficient_2000:
 % ? unused Library catalog ("ScienceDirect")
-% ? unused Url ("https://www.sciencedirect.com/science/article/pii/S0045782599003898")
+% ? unused URL ("https://www.sciencedirect.com/science/article/pii/S0045782599003898")
 
 @misc{green_re_2024,
   title = {Re: {{Increasing}} the Size of the Interface/Contains Stack - {{Intel Community}}},
@@ -62,7 +74,7 @@
 }
 % == BibTeX quality report for henon_numerical_1982:
 % ? unused Library catalog ("ScienceDirect")
-% ? unused Url ("https://www.sciencedirect.com/science/article/pii/0167278982900343")
+% ? unused URL ("https://www.sciencedirect.com/science/article/pii/0167278982900343")
 
 @book{kreyszig_advanced_1993,
   title = {Advanced Engineering Mathematics},
@@ -85,7 +97,6 @@
   year = 2013,
   edition = {Second edition},
   publisher = {Lulu Com},
-  address = {Place of publication not identified},
   url = {https://people.cs.gmu.edu/~sean/book/metaheuristics/},
   isbn = {978-1-300-54962-8},
   annotation = {OCLC: 900622585}
@@ -139,7 +150,7 @@
 % == BibTeX quality report for putko_approach_2001:
 % ? Title looks like it was stored in title-case in Zotero
 % ? unused Library catalog ("Silverchair")
-% ? unused Url ("https://doi.org/10.1115/1.1446068")
+% ? unused URL ("https://doi.org/10.1115/1.1446068")
 
 @article{roy_review_2005,
   title = {Review of Code and Solution Verification Procedures for Computational Simulation},
@@ -154,7 +165,7 @@
 }
 % == BibTeX quality report for roy_review_2005:
 % ? unused Library catalog ("ScienceDirect")
-% ? unused Url ("https://www.sciencedirect.com/science/article/pii/S0021999104004619")
+% ? unused URL ("https://www.sciencedirect.com/science/article/pii/S0021999104004619")
 
 @article{ruby_equivalent_2000,
   title = {Equivalent Mass of a Coil Spring},
@@ -169,7 +180,7 @@
 }
 % == BibTeX quality report for ruby_equivalent_2000:
 % ? unused Library catalog ("Silverchair")
-% ? unused Url ("https://doi.org/10.1119/1.880475")
+% ? unused URL ("https://doi.org/10.1119/1.880475")
 
 @misc{shooting_chrony_inc_quick_2016,
   title = {Quick Set-up Instructions for: {{F-1}}, {{M-1}}, {{Archery}}, {{Paintball-Chrony}}\textregistered, \& Respective {{Master Chrony}}\textregistered{} Units for These Models},
@@ -194,7 +205,7 @@
 % == BibTeX quality report for trettel_btrettelflt_2026:
 % ? Title looks like it was stored in title-case in Zotero
 % ? unused Library catalog ("GitHub")
-% ? unused originalDate ("2023-09-27T01:30:24Z")
+% ? unused Original date ("2023-09-27T01:30:24Z")
 % ? unused Programming language ("Fortran")
 
 @misc{trettel_compile-time_2025,

commit eba44d46ea66e5c55339d35466032ed53c0f46a8

fix: Made plunger impact more robust to floating point error. Fixes: b1441af060fd512d8ef32988f12acc30fba8ec0e ("fix: Made effective inverse mass zero for stationary x control volumes. Fixed the name of a variable."), e33b449bc73d3254cc36a63f5fd853cc96b5dfbb ("feat: Made a coefficient of restitution of zero work.")

src/cva.f90
@@ -2110,7 +2110,7 @@ pure subroutine get_sys_at_x(t_old, dt, i_cv_x_event, x_event, sys_old, sys_new,
     t = t_old + dt_i
 end subroutine get_sys_at_x
 
-!tripwire$ begin 6CB60BAD Update `\secref{plunger-impact}` of theory.tex.
+!tripwire$ begin 4FFEC573 Update `\secref{plunger-impact}` of theory.tex.
 pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact)
     ! Set `sys_before_impact` to the instant immediately after plunger impact.
     ! `sys_before_impact` is right before plunger impact occurs (plunger velocity has no changed yet).
@@ -2120,6 +2120,7 @@ pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact)
     type(cv_system_type), allocatable, intent(out) :: sys_after_impact
     
     integer :: i_cv_mirror
+    type(si_inverse_mass) :: rm_p_eff
     
     call assert(sys_before_impact%cv(i_cv)%cor%v%v >= 0.0_WP, "cva (get_sys_after_impact): cor >= 0 violated")
     call assert(sys_before_impact%cv(i_cv)%cor%v%v <= 1.0_WP, "cva (get_sys_after_impact): cor <= 1 violated")
@@ -2132,11 +2133,11 @@ pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact)
         sys_after_impact%cv(i_cv_mirror)%x_dot = -sys_after_impact%cv(i_cv)%x_dot
     end if
     
-    if (.not. is_close(sys_after_impact%cv(i_cv)%rm_p%v%v, 0.0_WP)) then
+    rm_p_eff = sys_after_impact%cv(i_cv)%rm_p_eff()
+    if (.not. is_close(rm_p_eff%v%v, 0.0_WP)) then
         sys_after_impact%cv(i_cv)%e_m = sys_before_impact%cv(i_cv)%e_m &
-                                            + (0.5_WP/sys_after_impact%cv(i_cv)%rm_p_eff()) &
-                                                *(square(sys_before_impact%cv(i_cv)%x_dot) &
-                                                    - square(sys_after_impact%cv(i_cv)%x_dot))
+                                            + (0.5_WP/rm_p_eff)*(square(sys_before_impact%cv(i_cv)%x_dot) &
+                                                                    - square(sys_after_impact%cv(i_cv)%x_dot))
         
         ! This is a fair bit of a hack.
         ! If the coefficient of restitution is zero, the plunger head right after impact is at `x_min`.
@@ -2145,7 +2146,8 @@ pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact)
         ! Preventing all plunger motion by making the effective mass infinite is one way around this.
         ! TODO: It would be better to add a force active only if `is_close(cv%x%v%v, cv%x_min%v%v)` to keep the plunger stationary.
         ! Then if the pressure in the CV overcomes the counteracting force, the plunger can start moving again.
-        if (is_close(sys_after_impact%cv(i_cv)%x_dot%v%v, 0.0_WP)) then
+        ! The `abs_tol` is needed to prevent stalling and is even more of a hack.
+        if (is_close(sys_after_impact%cv(i_cv)%x_dot%v%v, 0.0_WP, abs_tol=1.0e-15_WP)) then
             call sys_after_impact%cv(i_cv)%rm_p%v%init_const(0.0_WP, size(sys_after_impact%cv(i_cv)%rm_p%v%d))
         end if
     end if