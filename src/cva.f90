btrettel/blastersim

commit 8f270aca44b7c9d650b84fa4404b505b3a800016

feat: Added recovery time to the exponential backoff adaptive time step algorithm.
feat: Simulation progress and events are now printed to standard output.

docs/theory.tex
@@ -46,9 +46,9 @@ This capitalization convention is not followed in BlasterSim's source code.
 See \secref{conventions} for a discussion of the capitalization convention followed in the source code.
 Because each variable in BlasterSim's source code is given a unit (see \secref{unit-checking}), whether a quantity is extensive or intensive can be determined from the units for that quantity, if unclear.
 
-In the inputs, as diameters are provided, BlasterSim assumes that each control volume is cylindrical.
-However, internally, BlasterSim uses only control volume cross-sectional area, not diameter.
-BlasterSim internally makes no assumptions about the shape of each control volume, so BlasterSim can be extended to other control volume shapes in the future if desired, or an equivalent diameter with the intended cross-sectional area can be used in the meantime.
+In the inputs, as diameters are provided, BlasterSim assumes that each control volume is cylindrical with a circular or annular cross-section.
+However, internally, BlasterSim uses only control volume cross-sectional area, not a diameter.
+BlasterSim internally makes no assumptions about the shape of the cross-section of each control volume, so BlasterSim can be extended to other control volume shapes in the future if desired, or an equivalent diameter with the intended cross-sectional area can be used in the meantime.
 
 \subsection{Conservation laws}\label{conservation-laws}
 
@@ -320,7 +320,7 @@ The effective friction pressure $p_{\text{f},i}$ then will switch between static
     \end{cases}\label{eq:effective friction pressure cases}
 \end{equation}
 
-\textbf{BlasterSim does not use this model} because it is not smooth.
+\textbfwarning{BlasterSim does not use this model} because it is not smooth.
 Smooth functions are useful for automatic differentiation (see~\secref{autodiff}) and beneficial for numerical stability.
 Also, the use of strict floating point equality in the $\dot{x}_i = 0$ branch can lead to numerical reproducibility issues.
 BlasterSim uses a smoothed version of this model which avoids these problems.
@@ -434,11 +434,15 @@ A simple 4th order Runge-Kutta scheme~\cite[p.~1081]{kreyszig_advanced_1993} is 
 In the interior ballistics calculations, the time step ($\Delta t$/\texttt{dt}) is typically constant (the default is $\dtdefault$~s), except in certain circumstances:
 \begin{itemize}
 \item When an event like a plunger impact or projectile leaving the barrel is detected.
-In these cases, the time step is adjusted to closely determine the time of the event using a secant method variation of H\'{e}non's trick~\cite{henon_numerical_1982}.
-\item When between two time steps, the total system mass deviates by more than $\dtbackoffmasstolerance\%$ or the total system energy deviates by more than $\dtbackoffenergytolerance\%$, the time step is multiplied by $\dtbackofffactor$ to try to maintain strong conservation (exponential backoff).
+In these cases, the next time step is adjusted to closely determine the time of the event using a secant method variation of H\'{e}non's trick~\cite{henon_numerical_1982}.
+Subsequent time steps are unchanged.
+\item When between two time steps, the total system mass deviates by more than $\dtbackoffmasstolerance\%$ or the total system energy deviates by more than $\dtbackoffenergytolerance\%$, the time step is multiplied by $\dtbackoffconservation$ to try to maintain strong conservation (exponential backoff).
 This adaptivity is not enough in some instances, so it may sometimes be necessary for the user to reduce the time step.
-\item When plunger impact occurs, the time step is multiplied by $\dtimpactfactor$ as the plunger dynamics after impact may be faster.
+\item When plunger impact occurs, the time step is multiplied by $\dtbackoffimpact$ as the plunger dynamics after impact may be faster.
 This is particularly true after multiple plunger impacts, which can happen in some cases.
+\item If the time step is unchanged due to conservation or plunger impact for $\dtrecoveryiterations$ time iterations, then the time step is divided by $\dtbackoffimpact$ to increase its value.
+Every subsequent $\dtrecoveryiterations$ time iterations without increases to the time step have the same increase.
+The time step will not increase above the time step set for the simulation.
 \end{itemize}
 
 % TODO: Discuss exterior ballistics time integration.

docs/usage.tex
@@ -43,6 +43,13 @@ If BlasterSim is placed in your current directory, you can run BlasterSim with a
 Using the example input file in \secref{springer-example} returns the following:
 \lstinputlisting[breaklines=true]{blastersim-out-2.txt}
 
+BlasterSim will print out simulation progress and events to standard output.
+The \texttt{i=0} line shows the current iteration number (\texttt{i}), simulation time (\texttt{t}), and \textbfwarning{next} time step value (\texttt{dt}).
+Every $\printfrequency$ iterations, a line showing the iteration number, simulation time, and next time step value is printed.
+Most simulations will not reach $\printfrequency$ iterations, so it is normal to not see this message.
+The message exists to show that BlasterSim is making progress on the simulation.
+When an event like plunger impact or time step adjustment occurs, BlasterSim will print the iteration number, simulation time, next time step value, and a brief message describing the event.
+
 \section{Time trace output to CSV file}\label{csv}
 
 After BlasterSim completes, a CSV spreadsheet with detailed data the simulation is written.
@@ -138,7 +145,12 @@ A value of -2 and below indicates that an event that requires special treatment 
     BlasterSim will terminate.
     \item[return code -3:] A plunger has impacted the end of the plunger tube.
     BlasterSim will continue to the next time step.
-    \item[return code -4:] The time step was multiplied by $\dtbackofffactor$ due to a mass or energy conservation tolerance being violated.
+    The time step is multiplied by $\dtbackoffimpact$.
+    See \secref{time-integration}.
+    \item[return code -4:] The time step was multiplied by $\dtbackoffconservation$ due to a mass or energy conservation tolerance being violated.
+    See \secref{time-integration}.
+    BlasterSim will continue to the next time step.
+    \item[return code -5:] The time step was divided by $\dtbackoffconservation$ (making it larger) as it has been $\dtrecoveryiterations$ time steps since the time step was last changed and perhaps a larger time step is now acceptable.
     See \secref{time-integration}.
     BlasterSim will continue to the next time step.
     \textbfwarning{If another event or error occurred during this time step, that return code will override this return code.}
@@ -209,13 +221,14 @@ In the example namelist input file in this section, this would correspond to the
 
 BlasterSim creates 3 control volumes for a pneumatic gun:
 \begin{enumerate}
-\item Control volume 1: barrel
-\item Control volume 2: pressure chamber
-\item Control volume 3: atmospheric side of the barrel
+\item barrel
+\item pressure chamber
+\item atmospheric side of the barrel
 \end{enumerate}
 
 As shown in \figref{springer arbitrary time}, the dead volume is contained only in the barrel in the pneumatic mode.
 BlasterSim treats the barrel as cylindrical in the cross-sectional area calculation only.
+Internally, the dead volume $V_\text{dead}$ is converted to the projectile starting position $x_0$.
 
 \input{pneumatic-figures.tex}
 
@@ -235,10 +248,10 @@ If there is a corresponding typeset variable, the notation for that variable is 
 
 BlasterSim creates 3 control volumes for a springer:
 \begin{enumerate}
-\item Control volume 1: barrel
-\item Control volume 2: plunger tube
-\item Control volume 3: atmospheric side of the barrel
-\item Control volume 4: atmospheric side of the plunger tube
+\item barrel
+\item plunger tube
+\item atmospheric side of the barrel
+\item atmospheric side of the plunger tube
 \end{enumerate}
 
 As shown in \figref{springer arbitrary time}, the dead volume is evenly split between the barrel and plunger tube in the springer mode.

src/cva.f90
@@ -57,25 +57,26 @@ integer, public, parameter :: MAX_CV_TYPE    = 2
 
 integer, public, parameter :: SUCCESS_RC = 0
 
-!tripwire$ begin 24EDE6E5 Update \secref{run-time-checks}, \secref{csv} and `actual_rc` in geninput_*.nml.
-integer, public, parameter :: DT_CHANGED_RUN_RC             = -4
-integer, public, parameter :: X_LT_X_MIN_RUN_RC             = -3
-integer, public, parameter :: X_GE_X_STOP_RUN_RC            = -2
-integer, public, parameter :: CONTINUE_RUN_RC               = -1
-integer, public, parameter :: TIMEOUT_RUN_RC                = 1
-integer, public, parameter :: NEGATIVE_CV_M_TOTAL_RUN_RC    = 2
-integer, public, parameter :: NEGATIVE_CV_TEMP_RUN_RC       = 3
-integer, public, parameter :: MASS_TOLERANCE_RUN_RC         = 4
-integer, public, parameter :: ENERGY_TOLERANCE_RUN_RC       = 5
-integer, public, parameter :: MASS_DERIV_TOLERANCE_RUN_RC   = 6
-integer, public, parameter :: ENERGY_DERIV_TOLERANCE_RUN_RC = 7
-integer, public, parameter :: IDEAL_EOS_RUN_RC              = 8
-integer, public, parameter :: MIRROR_X_TOLERANCE_RUN_RC     = 9
-integer, public, parameter :: NEGATIVE_CV_X_RUN_RC          = 10
-integer, public, parameter :: MAX_ITERS_TIME_LOOP_RUN_RC    = 11
-integer, public, parameter :: MAX_ITERS_GET_SYS_AT_X_RUN_RC = 12
-integer, public, parameter :: RK_STAGE_NEGATIVE_MASS_RC     = 13
-integer, public, parameter :: RK_STAGE_NEGATIVE_ENERGY_RC   = 14
+!tripwire$ begin A2CE05BB Update \secref{run-time-checks}, \secref{csv} and `actual_rc` in geninput_*.nml.
+integer, public, parameter :: DT_CHANGED_RECOVERY_RUN_RC     = -5
+integer, public, parameter :: DT_CHANGED_CONSERVATION_RUN_RC = -4
+integer, public, parameter :: X_LT_X_MIN_RUN_RC              = -3
+integer, public, parameter :: X_GE_X_STOP_RUN_RC             = -2
+integer, public, parameter :: CONTINUE_RUN_RC                = -1
+integer, public, parameter :: TIMEOUT_RUN_RC                 = 1
+integer, public, parameter :: NEGATIVE_CV_M_TOTAL_RUN_RC     = 2
+integer, public, parameter :: NEGATIVE_CV_TEMP_RUN_RC        = 3
+integer, public, parameter :: MASS_TOLERANCE_RUN_RC          = 4
+integer, public, parameter :: ENERGY_TOLERANCE_RUN_RC        = 5
+integer, public, parameter :: MASS_DERIV_TOLERANCE_RUN_RC    = 6
+integer, public, parameter :: ENERGY_DERIV_TOLERANCE_RUN_RC  = 7
+integer, public, parameter :: IDEAL_EOS_RUN_RC               = 8
+integer, public, parameter :: MIRROR_X_TOLERANCE_RUN_RC      = 9
+integer, public, parameter :: NEGATIVE_CV_X_RUN_RC           = 10
+integer, public, parameter :: MAX_ITERS_TIME_LOOP_RUN_RC     = 11
+integer, public, parameter :: MAX_ITERS_GET_SYS_AT_X_RUN_RC  = 12
+integer, public, parameter :: RK_STAGE_NEGATIVE_MASS_RC      = 13
+integer, public, parameter :: RK_STAGE_NEGATIVE_ENERGY_RC    = 14
 !integer, public, parameter :: X_BLOW_UP_RUN_RC              = 
 !integer, public, parameter :: X_DOT_BLOW_UP_RUN_RC          = 
 !integer, public, parameter :: M_BLOW_UP_RUN_RC              = 
@@ -89,12 +90,15 @@ integer, public, parameter :: MAX_ITERS_GET_SYS_AT_X = 50
 integer, public, parameter :: HEADER_ROW_TYPE = 1
 integer, public, parameter :: NUMBER_ROW_TYPE = 2
 
-!tripwire$ begin 64C5C07A Update \secref{time-integration}.
-real(WP), public, parameter :: DT_BACKOFF_FACTOR           = 0.5_WP
+!tripwire$ begin EBBA5FD1 Update \secref{time-integration}.
+real(WP), public, parameter :: DT_BACKOFF_CONSERVATION     = 0.5_WP
 real(WP), public, parameter :: DT_BACKOFF_MASS_TOLERANCE   = 1.0e-8_WP
 real(WP), public, parameter :: DT_BACKOFF_ENERGY_TOLERANCE = 1.0e-8_WP
-real(WP), public, parameter :: DT_IMPACT_FACTOR            = 0.9_WP
+real(WP), public, parameter :: DT_BACKOFF_IMPACT           = 0.95_WP
+integer, public, parameter  :: DT_RECOVERY_ITERATIONS      = 1000
 !tripwire$ end
+
+integer, public, parameter :: PRINT_FREQUENCY = 10000
 
 type, public :: cv_type ! control volume
     ! time varying
@@ -1681,6 +1685,7 @@ end subroutine set_run_config
 subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     use, intrinsic :: iso_fortran_env, only: ERROR_UNIT
     use prec, only: CL
+    use convert, only: CONVERT_S_TO_MS
     
     type(run_config_type), intent(in)              :: config
     type(cv_system_type), allocatable, intent(in)  :: sys_start
@@ -1691,7 +1696,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     type(cv_system_type), allocatable :: sys_old, sys_new, sys_temp, sys_event
     
     character(len=CL)     :: error_message
-    integer               :: n_d, i, csv_unit, rc_get_sys_at_x, rc_time_step
+    integer               :: n_d, i, csv_unit, rc_get_sys_at_x, rc_time_step, i_last_dt_change
     type(si_time)         :: t, t_old, dt
     logical               :: exit_time_loop, stop_at_first_event_
     
@@ -1706,6 +1711,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     sys_old = sys_start
     call t%v%init_const(0.0_WP, n_d)
     i = 0
+    i_last_dt_change = -huge(1)
     
     if (config%csv_output) then
         open(newunit=csv_unit, action="write", status="replace", position="rewind", &
@@ -1723,6 +1729,8 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     
     dt = config%dt
     exit_time_loop = .false.
+    if (.not. config%const_dt) write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", &
+                                    CONVERT_S_TO_MS*t%v%v, " ms dt=", CONVERT_S_TO_MS*dt%v%v, " ms"
     time_loop: do
         call calculate_next_time_step(sys_old, t, dt, sys_new, rc_time_step)
         if (rc_time_step /= SUCCESS_RC) then
@@ -1769,13 +1777,30 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
         
         if (i >= MAX_ITERS_TIME_LOOP) status%rc = MAX_ITERS_TIME_LOOP_RUN_RC
         
-        if (.not. config%const_dt) call adapt_dt(sys_old, sys_new, dt, status%rc)
+        if (.not. config%const_dt) call adapt_dt(i, config%dt, sys_old, sys_new, dt, i_last_dt_change, status%rc)
         
         if (config%csv_output .and. &
                 ((mod(i, config%csv_frequency) == 0) .or. (status%rc < CONTINUE_RUN_RC))) then
             ! `status%rc < CONTINUE_RC` condition: Write all non-normal events to the CSV file for logging.
             call write_csv_row(csv_unit, sys_new, t, status, NUMBER_ROW_TYPE)
         end if
+        
+        ! This is after `event_case` as `DT_CHANGED_CONSERVATION_RUN_RC` isn't set until after `event_case`.
+        select case (status%rc)
+            case (X_LT_X_MIN_RUN_RC)
+                write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", CONVERT_S_TO_MS*t%v%v, " ms dt=", &
+                        CONVERT_S_TO_MS*dt%v%v, " ms: plunger impact, dt decreased"
+            case (DT_CHANGED_CONSERVATION_RUN_RC)
+                write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", CONVERT_S_TO_MS*t%v%v, " ms dt=", &
+                        CONVERT_S_TO_MS*dt%v%v, " ms: dt decreased due to conservation error"
+            case (DT_CHANGED_RECOVERY_RUN_RC)
+                write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", CONVERT_S_TO_MS*t%v%v, " ms dt=", &
+                        CONVERT_S_TO_MS*dt%v%v, " ms: dt increased"
+            case default
+                if ((mod(i, PRINT_FREQUENCY) == 0) .and. (.not. config%const_dt)) &
+                        write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", &
+                                    CONVERT_S_TO_MS*t%v%v, " ms dt=", CONVERT_S_TO_MS*dt%v%v, " ms"
+        end select
         
         if ((status%rc >= SUCCESS_RC) &
                 .or. exit_time_loop &
@@ -2376,20 +2401,23 @@ subroutine write_csv_row(csv_unit, sys, t, status, row_type)
 end subroutine write_csv_row
 !tripwire$ end
 
-!tripwire$ begin 58D3D342 Update \secref{time-integration}.
-pure subroutine adapt_dt(sys_old, sys_new, dt, rc)
+!tripwire$ begin 9311A9B6 Update \secref{time-integration}.
+pure subroutine adapt_dt(i, config_dt, sys_old, sys_new, dt, i_last_dt_change, rc)
     ! Adaptive time stepping based on conservation metrics and exponential backoff.
     
+    integer, intent(in)                           :: i
+    type(si_time), intent(in)                     :: config_dt
     type(cv_system_type), allocatable, intent(in) :: sys_old, sys_new
     type(si_time), intent(in out)                 :: dt
-    integer, intent(in out)                       :: rc
+    integer, intent(in out)                       :: i_last_dt_change, rc
     
     type(unitless)  :: rel_delta
     type(si_mass)   :: m_old
     type(si_energy) :: e_old
     
     if (rc == X_LT_X_MIN_RUN_RC) then
-        dt = DT_IMPACT_FACTOR*dt
+        dt = DT_BACKOFF_IMPACT*dt
+        i_last_dt_change = i
         return
     end if
     
@@ -2397,8 +2425,9 @@ pure subroutine adapt_dt(sys_old, sys_new, dt, rc)
     call assert(m_old%v%v > 0.0_WP, "cva (adapt_dt): m_old must be greater than zero", print_real=[m_old%v%v])
     rel_delta = abs(sys_new%m_total() - m_old) / m_old
     if (rel_delta%v%v > DT_BACKOFF_MASS_TOLERANCE) then
-        dt = DT_BACKOFF_FACTOR*dt
-        if (rc == CONTINUE_RUN_RC) rc = DT_CHANGED_RUN_RC
+        dt = DT_BACKOFF_CONSERVATION*dt
+        if (rc == CONTINUE_RUN_RC) rc = DT_CHANGED_CONSERVATION_RUN_RC
+        i_last_dt_change = i
         return
     end if
     
@@ -2406,12 +2435,20 @@ pure subroutine adapt_dt(sys_old, sys_new, dt, rc)
     call assert(e_old%v%v > 0.0_WP, "cva (adapt_dt): e_old must be greater than zero", print_real=[e_old%v%v])
     rel_delta = abs(sys_new%e_total() - e_old) / e_old
     if (rel_delta%v%v > DT_BACKOFF_ENERGY_TOLERANCE) then
-        dt = DT_BACKOFF_FACTOR*dt
-        if (rc == CONTINUE_RUN_RC) rc = DT_CHANGED_RUN_RC
+        dt = DT_BACKOFF_CONSERVATION*dt
+        if (rc == CONTINUE_RUN_RC) rc = DT_CHANGED_CONSERVATION_RUN_RC
+        i_last_dt_change = i
         return
     end if
     
     ! TODO: derivatives
+    
+    if ((i > (i_last_dt_change + DT_RECOVERY_ITERATIONS)) &
+                .and. (.not. is_close(dt%v%v, config_dt%v%v))) then
+        dt = max(config_dt, dt/max(DT_BACKOFF_IMPACT, DT_BACKOFF_MASS_TOLERANCE))
+        if (rc == CONTINUE_RUN_RC) rc = DT_CHANGED_RECOVERY_RUN_RC
+        i_last_dt_change = i
+    end if
 end subroutine adapt_dt
 !tripwire$ end
 

test/test_cva.f90
@@ -70,7 +70,8 @@ subroutine write_defaults()
     use cva, only: DT_DEFAULT, T_STOP_DEFAULT, COR_DEFAULT, &
                     MASS_TOLERANCE, ENERGY_TOLERANCE, MASS_DERIV_TOLERANCE, ENERGY_DERIV_TOLERANCE, &
                     MIRROR_X_TOLERANCE, MAX_ITERS_TIME_LOOP, MAX_ITERS_GET_SYS_AT_X, CSV_FREQUENCY_DEFAULT, &
-                    DT_BACKOFF_FACTOR, DT_BACKOFF_MASS_TOLERANCE, DT_BACKOFF_ENERGY_TOLERANCE, DT_IMPACT_FACTOR
+                    DT_BACKOFF_CONSERVATION, DT_BACKOFF_MASS_TOLERANCE, DT_BACKOFF_ENERGY_TOLERANCE, &
+                    DT_BACKOFF_IMPACT, DT_RECOVERY_ITERATIONS, PRINT_FREQUENCY
     use gasdata, only: P_ATM, TEMP_ATM, TEMP_0
     use convert, only: CONVERT_C_TO_K, CONVERT_PA_TO_KPA
     use io, only: write_latex_engineering
@@ -94,10 +95,12 @@ subroutine write_defaults()
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\maxiterstimeloop}{", MAX_ITERS_TIME_LOOP, "}"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\maxitersgetsysatx}{", MAX_ITERS_GET_SYS_AT_X, "}"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\csvfrequencydefault}{", CSV_FREQUENCY_DEFAULT, "}"
-    write(unit=tex_unit, fmt="(a, f3.1, a)") "\newcommand*{\dtbackofffactor}{", DT_BACKOFF_FACTOR, "}"
+    write(unit=tex_unit, fmt="(a, f3.1, a)") "\newcommand*{\dtbackoffconservation}{", DT_BACKOFF_CONSERVATION, "}"
     call write_latex_engineering(tex_unit, 100.0_WP*DT_BACKOFF_MASS_TOLERANCE, "dtbackoffmasstolerance", "f4.1")
     call write_latex_engineering(tex_unit, 100.0_WP*DT_BACKOFF_ENERGY_TOLERANCE, "dtbackoffenergytolerance", "f4.1")
-    write(unit=tex_unit, fmt="(a, f3.1, a)") "\newcommand*{\dtimpactfactor}{", DT_IMPACT_FACTOR, "}"
+    write(unit=tex_unit, fmt="(a, f3.1, a)") "\newcommand*{\dtbackoffimpact}{", DT_BACKOFF_IMPACT, "}"
+    write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\dtrecoveryiterations}{", DT_RECOVERY_ITERATIONS, "}"
+    write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\printfrequency}{", PRINT_FREQUENCY, "}"
     close(tex_unit)
 end subroutine write_defaults
 

app/blastersim.f90
@@ -65,7 +65,7 @@ end block nml_blk
 
 call run(config, sys_start, sys_end, status)
 
-!tripwire$ begin 9EDE37E1 Update `\secref{run-time-checks}` of verval.tex.
+!tripwire$ begin 8E6189A7 Update `\secref{run-time-checks}` of verval.tex.
 if (status%rc < SUCCESS_RC) then
     write(unit=OUTPUT_UNIT, fmt="(a)") "SUCCESS!"
     write(unit=OUTPUT_UNIT, fmt="(a, f0.2, a)") "muzzle velocity: ", sys_end%cv(I_BARREL)%x_dot%v%v, " m/s"
@@ -106,8 +106,8 @@ else
             call refer_to_docs()
             stop EX_SOFTWARE, quiet=.true.
         case (MAX_ITERS_TIME_LOOP_RUN_RC, MAX_ITERS_GET_SYS_AT_X_RUN_RC)
-            write(unit=ERROR_UNIT, fmt="(2a)") "Maximum number of iterations exceeded. ", &
-                                                "This is a bug that should be reported."
+            write(unit=ERROR_UNIT, fmt="(2a)") "Maximum number of iterations reached. ", &
+                                                "This might be a bug or possibly the projectile will not leave the barrel."
             call refer_to_docs()
             stop EX_SOFTWARE, quiet=.true.
         case (RK_STAGE_NEGATIVE_MASS_RC, RK_STAGE_NEGATIVE_ENERGY_RC)

commit d1566d8971966683654185e1bafcbe1d417e4b1a

fix: Made recovery time feature actually work properly.
Adjusted the exponential backoff algorithm's parameters a bit in the process.

Fixes: 8f270aca44b7c9d650b84fa4404b505b3a800016 ("feat: Added recovery time to the exponential backoff adaptive time step algorithm.")

docs/theory.tex
@@ -440,7 +440,7 @@ Subsequent time steps are unchanged.
 This adaptivity is not enough in some instances, so it may sometimes be necessary for the user to reduce the time step.
 \item When plunger impact occurs, the time step is multiplied by $\dtbackoffimpact$ as the plunger dynamics after impact may be faster.
 This is particularly true after multiple plunger impacts, which can happen in some cases.
-\item If the time step is unchanged due to conservation or plunger impact for $\dtrecoveryiterations$ time iterations, then the time step is divided by $\dtbackoffimpact$ to increase its value.
+\item If the time step is unchanged due to conservation or plunger impact for $\dtrecoveryiterations$ time iterations, then the time step is multiplied by $\dtrecovery$.
 Every subsequent $\dtrecoveryiterations$ time iterations without increases to the time step have the same increase.
 The time step will not increase above the time step set for the simulation.
 \end{itemize}

docs/usage.tex
@@ -150,7 +150,7 @@ A value of -2 and below indicates that an event that requires special treatment 
     \item[return code -4:] The time step was multiplied by $\dtbackoffconservation$ due to a mass or energy conservation tolerance being violated.
     See \secref{time-integration}.
     BlasterSim will continue to the next time step.
-    \item[return code -5:] The time step was divided by $\dtbackoffconservation$ (making it larger) as it has been $\dtrecoveryiterations$ time steps since the time step was last changed and perhaps a larger time step is now acceptable.
+    \item[return code -5:] The time step was multiplied by $\dtrecovery$ as it has been $\dtrecoveryiterations$ time steps since the time step was last changed and perhaps a larger time step is now acceptable.
     See \secref{time-integration}.
     BlasterSim will continue to the next time step.
     \textbfwarning{If another event or error occurred during this time step, that return code will override this return code.}

src/cva.f90
@@ -90,12 +90,13 @@ integer, public, parameter :: MAX_ITERS_GET_SYS_AT_X = 50
 integer, public, parameter :: HEADER_ROW_TYPE = 1
 integer, public, parameter :: NUMBER_ROW_TYPE = 2
 
-!tripwire$ begin EBBA5FD1 Update \secref{time-integration}.
+!tripwire$ begin DEE2943D Update \secref{time-integration} and \secref{csv}.
 real(WP), public, parameter :: DT_BACKOFF_CONSERVATION     = 0.5_WP
 real(WP), public, parameter :: DT_BACKOFF_MASS_TOLERANCE   = 1.0e-8_WP
 real(WP), public, parameter :: DT_BACKOFF_ENERGY_TOLERANCE = 1.0e-8_WP
 real(WP), public, parameter :: DT_BACKOFF_IMPACT           = 0.95_WP
 integer, public, parameter  :: DT_RECOVERY_ITERATIONS      = 1000
+real(WP), public, parameter :: DT_RECOVERY                 = 2.0_WP
 !tripwire$ end
 
 integer, public, parameter :: PRINT_FREQUENCY = 10000
@@ -2401,7 +2402,7 @@ subroutine write_csv_row(csv_unit, sys, t, status, row_type)
 end subroutine write_csv_row
 !tripwire$ end
 
-!tripwire$ begin 9311A9B6 Update \secref{time-integration}.
+!tripwire$ begin E6D7EFE0 Update \secref{time-integration} and \secref{csv}.
 pure subroutine adapt_dt(i, config_dt, sys_old, sys_new, dt, i_last_dt_change, rc)
     ! Adaptive time stepping based on conservation metrics and exponential backoff.
     
@@ -2443,9 +2444,9 @@ pure subroutine adapt_dt(i, config_dt, sys_old, sys_new, dt, i_last_dt_change, r
     
     ! TODO: derivatives
     
-    if ((i > (i_last_dt_change + DT_RECOVERY_ITERATIONS)) &
+    if ((i >= (i_last_dt_change + DT_RECOVERY_ITERATIONS)) &
                 .and. (.not. is_close(dt%v%v, config_dt%v%v))) then
-        dt = max(config_dt, dt/max(DT_BACKOFF_IMPACT, DT_BACKOFF_MASS_TOLERANCE))
+        dt = min(config_dt, DT_RECOVERY*dt)
         if (rc == CONTINUE_RUN_RC) rc = DT_CHANGED_RECOVERY_RUN_RC
         i_last_dt_change = i
     end if

test/test_cva.f90
@@ -71,7 +71,7 @@ subroutine write_defaults()
                     MASS_TOLERANCE, ENERGY_TOLERANCE, MASS_DERIV_TOLERANCE, ENERGY_DERIV_TOLERANCE, &
                     MIRROR_X_TOLERANCE, MAX_ITERS_TIME_LOOP, MAX_ITERS_GET_SYS_AT_X, CSV_FREQUENCY_DEFAULT, &
                     DT_BACKOFF_CONSERVATION, DT_BACKOFF_MASS_TOLERANCE, DT_BACKOFF_ENERGY_TOLERANCE, &
-                    DT_BACKOFF_IMPACT, DT_RECOVERY_ITERATIONS, PRINT_FREQUENCY
+                    DT_BACKOFF_IMPACT, DT_RECOVERY_ITERATIONS, DT_RECOVERY, PRINT_FREQUENCY
     use gasdata, only: P_ATM, TEMP_ATM, TEMP_0
     use convert, only: CONVERT_C_TO_K, CONVERT_PA_TO_KPA
     use io, only: write_latex_engineering
@@ -100,6 +100,7 @@ subroutine write_defaults()
     call write_latex_engineering(tex_unit, 100.0_WP*DT_BACKOFF_ENERGY_TOLERANCE, "dtbackoffenergytolerance", "f4.1")
     write(unit=tex_unit, fmt="(a, f3.1, a)") "\newcommand*{\dtbackoffimpact}{", DT_BACKOFF_IMPACT, "}"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\dtrecoveryiterations}{", DT_RECOVERY_ITERATIONS, "}"
+    write(unit=tex_unit, fmt="(a, f3.1, a)") "\newcommand*{\dtrecovery}{", DT_RECOVERY, "}"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\printfrequency}{", PRINT_FREQUENCY, "}"
     close(tex_unit)
 end subroutine write_defaults