btrettel/blastersim

commit 03b101a8698a37bfe3c1163cb2f1337e87090b4e

feat: Made BlasterSim return an error for unrealistically large muzzle velocities.
docs: Moved all return codes to one section in the usage chapter.

app/blastersim.f90
@@ -16,7 +16,7 @@ use cva, only: run_config_type, cv_system_type, run_status_type, T_STOP_DEFAULT,
                     MASS_TOLERANCE_RUN_RC, ENERGY_TOLERANCE_RUN_RC, MASS_DERIV_TOLERANCE_RUN_RC, &
                     ENERGY_DERIV_TOLERANCE_RUN_RC, IDEAL_EOS_RUN_RC, MIRROR_X_TOLERANCE_RUN_RC, &
                     NEGATIVE_CV_X_RUN_RC, MAX_ITERS_TIME_LOOP_RUN_RC, MAX_ITERS_GET_SYS_AT_X_RUN_RC, &
-                    RK_STAGE_NEGATIVE_MASS_RC, RK_STAGE_NEGATIVE_ENERGY_RC
+                    RK_STAGE_NEGATIVE_MASS_RC, RK_STAGE_NEGATIVE_ENERGY_RC, MAX_VELOCITY_EXCEEDED_RC
 use stopcodes, only: EX_OK, EX_USAGE, EX_SOFTWARE
 use rev, only: TAG, REVISION_DATE, MODIFIED
 use checks, only: assert
@@ -65,7 +65,9 @@ end block nml_blk
 
 call run(config, sys_start, sys_end, status)
 
-!tripwire$ begin 8E6189A7 Update `\secref{run-time-checks}` of verval.tex.
+!tripwire$ begin BE34A827 Update `\secref{return-codes}` of usage.tex.
+call post_run_checks(sys_start, sys_end, rc)
+
 if (status%rc < SUCCESS_RC) then
     write(unit=OUTPUT_UNIT, fmt="(a)") "SUCCESS!"
     write(unit=OUTPUT_UNIT, fmt="(a, f0.2, a)") "muzzle velocity: ", sys_end%cv(I_BARREL)%x_dot%v%v, " m/s"
@@ -115,6 +117,12 @@ else
                     "Check whether d_e is too large, or possibly if dt is too large."
             call refer_to_docs()
             stop EX_USAGE, quiet=.true.
+        case (MAX_VELOCITY_EXCEEDED_RC)
+            write(unit=ERROR_UNIT, fmt="(a)") "Muzzle velocity exceeded what is physically possible. ", &
+                                                "This is a bug that should be reported."
+            write(unit=ERROR_UNIT, fmt="(a, f0.2, a)") "muzzle velocity: ", sys_end%cv(I_BARREL)%x_dot%v%v, " m/s"
+            call refer_to_docs()
+            stop EX_SOFTWARE, quiet=.true.
         case default
             write(unit=ERROR_UNIT, fmt="(a)") "Unknown error. This is a bug that should be reported."
             stop EX_SOFTWARE, quiet=.true.
@@ -126,7 +134,32 @@ contains
 
 subroutine refer_to_docs()
     write(unit=ERROR_UNIT, fmt="(a)") "Refer to BlasterSim User's Guide for possibly more information."
-    write(unit=ERROR_UNIT, fmt="(a)") "<http://trettel.us/blastersim/docs/verification.html#run-time-checks>"
+    write(unit=ERROR_UNIT, fmt="(a)") "<file:///home/ben/git/blastersim/docs/return-codes.html>"
 end subroutine refer_to_docs
+
+pure subroutine post_run_checks(sys_start, sys_end, rc)
+    use units
+    use io, only: I_SOURCE
+    use prec, only: WP
+    
+    type(cv_system_type), allocatable, intent(in) :: sys_start, sys_end
+    integer, intent(in out)                       :: rc
+    
+    type(unitless)    :: y(size(sys_start%cv(1)%m_k))
+    type(si_velocity) :: v_escape
+    
+    ! The reason this check isn't in `check_sys` is that it shouldn't apply to every CV, only the barrel.
+    
+    y = sys_start%cv(I_SOURCE)%y()
+    
+    ! corner_theory_1950 p. 364, eq. 74
+    ! seigel_theory_1965 p. 19, eq. 11-8
+    v_escape = 2.0_WP * sqrt(sys_start%cv(I_SOURCE)%gamma(y) * sys_start%cv(I_SOURCE)%r() * sys_start%cv(I_SOURCE)%temp()) &
+                            / (sys_start%cv(I_SOURCE)%gamma(y) - 1.0_WP)
+    
+    if ((rc < SUCCESS_RC) .and. (sys_end%cv(I_BARREL)%x_dot > v_escape)) then
+        rc = MAX_VELOCITY_EXCEEDED_RC
+    end if
+end subroutine post_run_checks
 
 end program blastersim

docs/blastersim.bib
@@ -97,7 +97,7 @@
   title = {Advanced Engineering Mathematics},
   author = {Kreyszig, Erwin},
   year = 1993,
-  edition = {7th ed},
+  edition = {7th},
   publisher = {Wiley},
   address = {New York},
   isbn = {978-0-471-55380-9}
@@ -143,7 +143,7 @@
   title = {Fundamentals of Engineering Thermodynamics},
   author = {Moran, Michael J. and Shapiro, Howard N.},
   year = 2008,
-  edition = {6th ed},
+  edition = {6th},
   publisher = {Wiley},
   address = {Hoboken, NJ},
   isbn = {978-0-471-78735-8}
@@ -175,7 +175,7 @@
   year = 2011,
   number = {ADA553634},
   institution = {Air Force Research Lab., Wright-Patterson AFB, OH. Propulsion Directorate.},
-  url = {https://apps.dtic.mil/sti/html/tr/ADA553634/}
+  url = {https://ntrl.ntis.gov/NTRL/dashboard/searchResults/titleDetail/ADA553634.xhtml}
 }
 % == BibTeX quality report for nurick_flow_2011:
 % ? Title looks like it was stored in title-case in Zotero
@@ -202,7 +202,7 @@
 
 @article{putko_approach_2001,
   title = {Approach for {{Input Uncertainty Propagation}} and {{Robust Design}} in {{CFD Using Sensitivity Derivatives}}},
-  author = {Putko, Michele M. and Taylor, Arthur C. , III and Newman, Perry A. and Green, Lawrence L.},
+  author = {Putko, Michele M. and Taylor, III, Arthur C. and Newman, Perry A. and Green, Lawrence L.},
   year = 2001,
   journal = {Journal of Fluids Engineering},
   volume = {124},
@@ -252,6 +252,22 @@
 % == BibTeX quality report for ruby_equivalent_2000:
 % ? unused Library catalog ("Silverchair")
 % ? unused URL ("https://doi.org/10.1119/1.880475")
+
+@book{seigel_theory_1965,
+  title = {The {{Theory}} of {{High Speed Guns}}},
+  author = {Seigel, Arnold E},
+  year = 1965,
+  series = {{{AGARDograph}}},
+  number = {91},
+  publisher = {AGARD},
+  url = {https://ntrl.ntis.gov/NTRL/dashboard/searchResults/titleDetail/AD475660.xhtml}
+}
+% == BibTeX quality report for seigel_theory_1965:
+% ? Title looks like it was stored in title-case in Zotero
+% ? unused Call number ("AD0475660")
+% ? unused Language ("en")
+% ? unused Library catalog ("Defense Technical Information Center")
+% ? unused Number of pages ("313")
 
 @misc{shooting_chrony_inc_quick_2016,
   title = {Quick Set-up Instructions for: {{F-1}}, {{M-1}}, {{Archery}}, {{Paintball-Chrony}}\textregistered, \& Respective {{Master Chrony}}\textregistered{} Units for These Models},

docs/chktexrc
@@ -19,6 +19,7 @@ UserWarn
     "internal ballistics" # write interior ballistics
     "external ballistics" # write exterior ballistics
     # "flow connection" # write flow restriction
+    section{BlasterSim # Everything here applies to BlasterSim, so it's not necessary to mention BlasterSim in headings
 ]
 
 UserWarnRegex

docs/dev.tex
@@ -134,7 +134,7 @@ Note that these Make commands will run some BlasterSim tests to generate some nu
 This will slow down compilation of the documentation
 For speed, once the documentation numbers are generated, these BlasterSim tests will not be run again when compiling the documentation unless BlasterSim's code changes.
 
-\section{BlasterSim source and documentation coding conventions}\label{conventions}
+\section{Coding conventions}\label{conventions}
 
 \begin{itemize}
 \item In cva.f90 and BlasterSim's core simulation procedures otherwise, all variables are in SI units.
@@ -150,8 +150,10 @@ I intend to slowly clean up and add these notes to this documentation.
 Note that this is different from convention used for intensive and extensive quantities in the typeset math in this documentation as discussed in \secref{notation}.
 Because each variable in BlasterSim's source code is given a unit (see \secref{unit-checking}), whether a quantity is extensive or intensive can be determined from the units for that quantity, if unclear.
 
-\item The control volume index for the barrel is \texttt{I\_BARREL}, which is set to 1.
+\item The control volume index for the barrel is \texttt{I\_BARREL}, which is set to $\ibarrel$.
 This convention allows BlasterSim to easily know which control volume is the barrel.
+
+\item The control volume index for the ``source'' (which control volume the propelling gas originates from, whether plunger tube or pressure chamber) is \texttt{I\_SOURCE}, which is set to $\isource$.
 
 \item \texttt{integer} variables named \texttt{rc} are used for return codes.
 0 or negative return codes indicate success, with 0 being normal success and negative numbers having a special context-dependent meaning.

docs/usage.tex
@@ -60,7 +60,7 @@ For example, in \secref{springer-example} the file name is springer-example.nml,
 By default, every $\csvfrequencydefault$th time step is written to the CSV file rather than every time step.
 Every time step would be too detailed and lead to large CSV files.
 How often the CSV file is written to can be changed with the \texttt{csv\_frequency} variable in the inputs.
-Additionally, all time steps with events that require special treatment from BlasterSim like the plunger hitting the end of the plunger tube in a springer will be written to the CSV file, and the type of event can be determined from the \texttt{rc} column. % TODO: link to where all `rc` values are defined once you list the negative ones
+Additionally, all time steps with events that require special treatment from BlasterSim like the plunger hitting the end of the plunger tube in a springer will be written to the CSV file, and the type of event can be determined from the \texttt{rc} column.
 
 The CSV file contains the following columns:
 \begin{itemize}
@@ -135,31 +135,67 @@ This should remain approximately constant and is present for debugging purposes.
 \end{itemize}
 \item \texttt{rc}: Return code of \texttt{run} subroutine.
 Integer.
-Values 1 and above indicate an error occurred and are discussed in \secref{run-time-checks}.
-A value of -2 and below indicates that an event that requires special treatment was detected (for example, the projectile leaving the barrel or the plunger impacting the end of the plunger tube), and BlasterSim may or may not terminate the simulation depending on the event.
-\begin{description}
-    \item[return code -1:] No errors or events were detected.
-    BlasterSim will continue to the next time step.
-    \item[return code -2:] A control volume's projectile/plunger has reached its termination position.
-    For example, this could be triggered if a projectile leaves the barrel.
-    BlasterSim will terminate.
-    \item[return code -3:] A plunger has impacted the end of the plunger tube with non-zero plunger velocity after impact.
-    BlasterSim will continue to the next time step.
-    The time step is multiplied by $\dtbackoffimpact$.
-    See \secref{time-integration}.
-    \item[return code -4:] The time step was multiplied by $\dtbackoffconservation$ due to a mass or energy conservation tolerance being violated.
-    See \secref{time-integration}.
-    BlasterSim will continue to the next time step.
-    \item[return code -5:] The time step was multiplied by $\dtrecovery$ as it has been $\dtrecoveryiterations$ time steps since the time step was last changed and perhaps a larger time step is now acceptable.
-    See \secref{time-integration}.
-    BlasterSim will continue to the next time step.
-    \textbfwarning{If another event or error occurred during this time step, that return code will override this return code.}
-    \item[return code -6:] A plunger has impacted the end of the plunger tube with zero plunger velocity after impact.
-    BlasterSim will continue to the next time step.
-\end{description}
+See \secref{return-codes} for details on the precise meaning of each code.
 \end{itemize}
 
-\section{BlasterSim inputs in general}\label{inputs-general}
+\section{Return codes}\label{return-codes}
+
+Return codes are printed in the CSV file and also printed when BlasterSim terminates with an error.
+Values 1 and above indicate an error occurred.
+
+Error return codes are listed below:
+\begin{description}
+\item[return code 1:] Whether the simulation ran for $\tstopdefault$~seconds without the projectile leaving the barrel, indicating that the projectile likely won't ever leave the barrel.
+\item[return code 2:] Whether a control volume has negative mass.
+\item[return code 3:] Whether a control volume has negative temperature.
+\item[return code 4:] Whether the total system mass deviates from the starting mass by more than $\masstolerance$.
+\item[return code 5:] Whether the total system energy deviates from the starting energy by more than $\energytolerance$.
+\item[return code 6:] If automatic differentiation is used, whether any component of the gradient of the total system mass deviates by more than $\massderivtolerance$ (regardless of the units).
+\item[return code 7:] If automatic differentiation is used, whether any component of the gradient of the total system energy deviates by more than $\energyderivtolerance$ (regardless of the units).
+\item[return code 8:] Whether the ideal gas equation of state has become inaccurate due to the pressure increasing above the critical pressure.
+\item[return code 9:] Whether the coordinates of any plunger become desynchronized between control volumes on either side of the mentioned plunger by more than $\mirrorxtolerance$~m.
+\item[return code 10:] Whether the coordinate of a projectile/plunger goes negative, indicating that the control volume has negative volume and that the projectile/plunger has not only impacted the end but gone through the end.
+\item[return code 11:] Whether the maximum number of time loop iterations ($\maxiterstimeloop$) was reached when time stepping.
+This indicates that BlasterSim was getting stuck at some point.
+Looking at the CSV output should indicate where BlasterSim was getting stuck for debugging.
+\item[return code 12:] Whether the maximum number of iterations ($\maxitersgetsysatx$) was reached when interpolating to a particular projectile/plunger location.
+This indicates that BlasterSim was unable to properly handle an event like projectile exit or a plunger impacting the end of the plunger tube.
+\item[return code 13:] Whether any species in a control volume had negative mass in a Runge-Kutta stage.
+This tends to occur when the equivalent diameter of the flow restriction is too large.
+Alternatively, the time step may be too large, however, the equivalent diameter should be checked first as the default time step should be acceptable in the vast majority of situations.
+\item[return code 14:] Whether any control volume had negative energy in a Runge-Kutta stage.
+This is similar to return code 13 and can be fixed in the same ways.
+\item[return code 15:] Whether the muzzle velocity exceeds what is believed to be physically possible.
+See \citep[p.~364, eq.~74]{corner_theory_1950} and \citep[p.~19, eq.~11-8]{seigel_theory_1965}.
+This check is performed only at the end of the simulation, not after each time step.
+\end{description}
+
+The thresholds used for some of these checks are somewhat arbitrary and are based on what conservatively seems to never cause problems in practice.
+
+The non-error return codes are listed below.
+A value of -2 and below indicates that an event that requires special treatment was detected (for example, the projectile leaving the barrel or the plunger impacting the end of the plunger tube), and BlasterSim may or may not terminate the simulation depending on the event.
+\begin{description}
+\item[return code -1:] No errors or events were detected.
+BlasterSim will continue to the next time step.
+\item[return code -2:] A control volume's projectile/plunger has reached its termination position.
+For example, this could be triggered if a projectile leaves the barrel.
+BlasterSim will terminate.
+\item[return code -3:] A plunger has impacted the end of the plunger tube with non-zero plunger velocity after impact.
+BlasterSim will continue to the next time step.
+The time step is multiplied by $\dtbackoffimpact$.
+See \secref{time-integration}.
+\item[return code -4:] The time step was multiplied by $\dtbackoffconservation$ due to a mass or energy conservation tolerance being violated.
+See \secref{time-integration}.
+BlasterSim will continue to the next time step.
+\item[return code -5:] The time step was multiplied by $\dtrecovery$ as it has been $\dtrecoveryiterations$ time steps since the time step was last changed and perhaps a larger time step is now acceptable.
+See \secref{time-integration}.
+BlasterSim will continue to the next time step.
+\textbfwarning{If another event or error occurred during this time step, that return code will override this return code.}
+\item[return code -6:] A plunger has impacted the end of the plunger tube with zero plunger velocity after impact.
+BlasterSim will continue to the next time step.
+\end{description}
+
+\section{Inputs in general}\label{inputs-general}
 
 BlasterSim uses Fortran namelist format input files.
 Namelist files are broken into groups and variables.

docs/verval.tex
@@ -19,34 +19,7 @@ These tests do not test that the governing equations are appropriate in the firs
 Every time step, BlasterSim performs internal consistency checks to alert the user if the simulation is becoming inaccurate in a detectable way.
 If an error is detected, BlasterSim terminates the simulation and prints out the return code.
 These checks are made in the \texttt{check\_sys} subroutine of cva.f90.
-The specific checks performed and the associated return codes include:
-\begin{description}
-\item[return code 1:] Whether the simulation ran for $\tstopdefault$~seconds without the projectile leaving the barrel, indicating that the projectile likely won't ever leave the barrel.
-\item[return code 2:] Whether a control volume has negative mass.
-\item[return code 3:] Whether a control volume has negative temperature.
-\item[return code 4:] Whether the total system mass deviates from the starting mass by more than $\masstolerance$.
-\item[return code 5:] Whether the total system energy deviates from the starting energy by more than $\energytolerance$.
-\item[return code 6:] If automatic differentiation is used, whether any component of the gradient of the total system mass deviates by more than $\massderivtolerance$ (regardless of the units).
-\item[return code 7:] If automatic differentiation is used, whether any component of the gradient of the total system energy deviates by more than $\energyderivtolerance$ (regardless of the units).
-\item[return code 8:] Whether the ideal gas equation of state has become inaccurate due to the pressure increasing above the critical pressure.
-\item[return code 9:] Whether the coordinates of any plunger become desynchronized between control volumes on either side of the mentioned plunger by more than $\mirrorxtolerance$~m.
-\item[return code 10:] Whether the coordinate of a projectile/plunger goes negative, indicating that the control volume has negative volume and that the projectile/plunger has not only impacted the end but gone through the end.
-\item[return code 11:] Whether the maximum number of time loop iterations ($\maxiterstimeloop$) was reached when time stepping.
-This indicates that BlasterSim was getting stuck at some point.
-Looking at the CSV output should indicate where BlasterSim was getting stuck for debugging.
-\item[return code 12:] Whether the maximum number of iterations ($\maxitersgetsysatx$) was reached when interpolating to a particular projectile/plunger location.
-This indicates that BlasterSim was unable to properly handle an event like projectile exit or a plunger impacting the end of the plunger tube.
-\item[return code 13:] Whether any species in a control volume had negative mass in a Runge-Kutta stage.
-This tends to occur when the equivalent diameter of the flow restriction is too large.
-Alternatively, the time step may be too large, however, the equivalent diameter should be checked first as the default time step should be acceptable in the vast majority of situations.
-\item[return code 14:] Whether any control volume had negative energy in a Runge-Kutta stage.
-This is similar to return code 13 and can be fixed in the same ways.
-\end{description}
-
-The thresholds used for some of these checks are somewhat arbitrary and are based on what conservatively seems to never cause problems in practice.
-
-For a discussion of return codes -1 and below, see \secref{csv} on the CSV output variable \texttt{rc}.
-These return codes indicate no errors were detected.
+See \secref{return-codes} for details.
 
 \subsection{Debugging run-time assertions}\label{assertions}
 

mk/before.mk
@@ -11,4 +11,4 @@ TESTNML = convert.nml cva.nml gasdata.nml io.nml validation.nml
 # All source files #
 ####################
 
-ALLSRC = app$(DIR_SEP)blastersim.f90 mk$(DIR_SEP)before.mk mk$(DIR_SEP)common.mk mk$(DIR_SEP)crayftn.mk mk$(DIR_SEP)depends.mk mk$(DIR_SEP)flang.mk mk$(DIR_SEP)gfortran.mk mk$(DIR_SEP)ifort_linux.mk mk$(DIR_SEP)ifort_windows.mk mk$(DIR_SEP)ifx_linux.mk mk$(DIR_SEP)ifx_windows.mk mk$(DIR_SEP)latex.mk mk$(DIR_SEP)lfortran.mk mk$(DIR_SEP)linux_2.mk mk$(DIR_SEP)linux_defaults.mk mk$(DIR_SEP)manual.mk mk$(DIR_SEP)nagfor.mk mk$(DIR_SEP)nvfortran.mk mk$(DIR_SEP)windows_2.mk mk$(DIR_SEP)windows_defaults.mk mk$(DIR_SEP)xlf.mk src$(DIR_SEP)checks.f90 src$(DIR_SEP)cli.f90 src$(DIR_SEP)convert.f90 src$(DIR_SEP)cva.f90 src$(DIR_SEP)debug.f90 src$(DIR_SEP)fmad.f90 src$(DIR_SEP)ga.f90 src$(DIR_SEP)gasdata.f90 src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 src$(DIR_SEP)geninput_springer.nml src$(DIR_SEP)genunits.nml src$(DIR_SEP)io.f90 src$(DIR_SEP)ooa.f90 src$(DIR_SEP)port.f90 src$(DIR_SEP)prec.f90 src$(DIR_SEP)purerng.f90 src$(DIR_SEP)release.f90 src$(DIR_SEP)stopcodes.f90 src$(DIR_SEP)timer.f90 src$(DIR_SEP)units.f90 src$(DIR_SEP)unittest.f90 src$(DIR_SEP)validation.f90 test$(DIR_SEP)test_convert.f90 test$(DIR_SEP)test_cva.f90 test$(DIR_SEP)test_gasdata.f90 test$(DIR_SEP)test_io.f90 test$(DIR_SEP)test_validation.f90
+ALLSRC = app$(DIR_SEP)blastersim.f90 mk$(DIR_SEP)before.mk mk$(DIR_SEP)common.mk mk$(DIR_SEP)crayftn.mk mk$(DIR_SEP)depends.mk mk$(DIR_SEP)flang.mk mk$(DIR_SEP)gfortran.mk mk$(DIR_SEP)ifort_linux.mk mk$(DIR_SEP)ifort_windows.mk mk$(DIR_SEP)ifx_linux.mk mk$(DIR_SEP)ifx_windows.mk mk$(DIR_SEP)latex.mk mk$(DIR_SEP)lfortran.mk mk$(DIR_SEP)linux_2.mk mk$(DIR_SEP)linux_defaults.mk mk$(DIR_SEP)manual.mk mk$(DIR_SEP)nagfor.mk mk$(DIR_SEP)nvfortran.mk mk$(DIR_SEP)windows_2.mk mk$(DIR_SEP)windows_defaults.mk mk$(DIR_SEP)xlf.mk src$(DIR_SEP)checks.f90 src$(DIR_SEP)cli.f90 src$(DIR_SEP)convert.f90 src$(DIR_SEP)cva.f90 src$(DIR_SEP)debug.f90 src$(DIR_SEP)fmad.f90 src$(DIR_SEP)ga.f90 src$(DIR_SEP)gasdata.f90 src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 src$(DIR_SEP)geninput_springer.nml src$(DIR_SEP)genunits.nml src$(DIR_SEP)io.f90 src$(DIR_SEP)linear_spring.f90 src$(DIR_SEP)ooa.f90 src$(DIR_SEP)port.f90 src$(DIR_SEP)prec.f90 src$(DIR_SEP)purerng.f90 src$(DIR_SEP)release.f90 src$(DIR_SEP)stopcodes.f90 src$(DIR_SEP)timer.f90 src$(DIR_SEP)units.f90 src$(DIR_SEP)unittest.f90 src$(DIR_SEP)validation.f90 test$(DIR_SEP)test_convert.f90 test$(DIR_SEP)test_cva.f90 test$(DIR_SEP)test_gasdata.f90 test$(DIR_SEP)test_io.f90 test$(DIR_SEP)test_validation.f90

mk/depends.mk
@@ -22,6 +22,8 @@ src$(DIR_SEP)gasdata.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)conve
 
 src$(DIR_SEP)io.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 src$(DIR_SEP)io.f90
 
+src$(DIR_SEP)linear_spring.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)linear_spring.f90
+
 src$(DIR_SEP)ooa.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)ooa.f90
 
 src$(DIR_SEP)port.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)port.f90

commit 07b37ba5b44bb530df3ec50b2a641d2a3a1ec3c8

fix: Made post_run_checks not crash for certain return codes.
Fixes: 03b101a8698a37bfe3c1163cb2f1337e87090b4e ("feat: Made BlasterSim return an error for unrealistically large muzzle velocities.")

app/blastersim.f90
@@ -157,8 +157,10 @@ pure subroutine post_run_checks(sys_start, sys_end, rc)
     v_escape = 2.0_WP * sqrt(sys_start%cv(I_SOURCE)%gamma(y) * sys_start%cv(I_SOURCE)%r() * sys_start%cv(I_SOURCE)%temp()) &
                             / (sys_start%cv(I_SOURCE)%gamma(y) - 1.0_WP)
     
-    if ((rc < SUCCESS_RC) .and. (sys_end%cv(I_BARREL)%x_dot > v_escape)) then
-        rc = MAX_VELOCITY_EXCEEDED_RC
+    if (rc < SUCCESS_RC) then
+        if (sys_end%cv(I_BARREL)%x_dot > v_escape) then
+            rc = MAX_VELOCITY_EXCEEDED_RC
+        end if
     end if
 end subroutine post_run_checks
 