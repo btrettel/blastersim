a7b28b37a14878028312113fd1091f16420a463d
Ben Trettel <12763077+btrettel@users.noreply.github.com>
2026-09-09 21:52:07 -0400

    build, fix: Added a file neglected from the previous commit.
    Fixes: 7326c8a04a3d3d1f10fb6d5c7046dcf8bba734ed ("build: Trying a fuzz testing build mode.")

---
+++ src/fuzz.f90
@@ -0,0 +1,15 @@
+! Module to make changes for different builds. Fuzz testing version.
+! Standard: Fortran 2018
+! Preprocessor: none
+! Author: Ben Trettel (<http://trettel.us/>)
+! Project: [BlasterSim](https://github.com/btrettel/blastersim)
+! License: [AGPLv3](https://www.gnu.org/licenses/agpl-3.0.en.html)
+
+module build
+
+implicit none
+private
+
+logical, public, parameter :: DEBUG = .true.
+
+end module build
