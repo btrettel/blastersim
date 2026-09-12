btrettel/blastersim

commit 7c0cd6e8a5e1b3024d57892977930ff0f738c8cd

fix: Made BlasterSim more robust to repeated plunger impacts.

src/cva.f90
@@ -55,8 +55,9 @@ integer, public, parameter :: NORMAL_CV_TYPE = 1
 integer, public, parameter :: MIRROR_CV_TYPE = 2
 integer, public, parameter :: MAX_CV_TYPE    = 2
 
-!tripwire$ begin 7DB15B07 Update \secref{return-codes} and `actual_rc` in geninput_*.nml.
-integer, public, parameter :: IMPACT_STOP_RUN_RC             = -6
+!tripwire$ begin D22EA909 Update \secref{return-codes} and `actual_rc` in geninput_*.nml.
+integer, public, parameter :: IMPACT_STOP_OTHER_RUN_RC       = -7
+integer, public, parameter :: IMPACT_STOP_VELOCITY_RUN_RC    = -6
 integer, public, parameter :: DT_CHANGED_RECOVERY_RUN_RC     = -5
 integer, public, parameter :: DT_CHANGED_CONSERVATION_RUN_RC = -4
 integer, public, parameter :: X_LT_X_MIN_RUN_RC              = -3
@@ -1700,9 +1701,10 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     type(cv_system_type), allocatable :: sys_old, sys_new, sys_temp, sys_event
     
     character(len=CL)     :: error_message
-    integer               :: n_d, i, csv_unit, rc_get_sys_at_x, rc_time_step, i_last_dt_change
+    integer               :: n_d, i, csv_unit, rc_get_sys_at_x, rc_time_step, i_last_dt_change, i_cv_x_event
     type(si_time)         :: t, t_old, dt
     logical               :: exit_time_loop, stop_at_first_event_
+    type(si_inverse_mass) :: rm_p_eff
     
     if (present(stop_at_first_event)) then
         stop_at_first_event_ = stop_at_first_event
@@ -1769,13 +1771,33 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
                     status%rc = rc_get_sys_at_x
                 end if
             case (X_LT_X_MIN_RUN_RC) ! -3
-                call get_sys_at_x(t_old, dt, status%i_cv(1), sys_old%cv(status%i_cv(1))%x_min, &
-                                    sys_old, sys_new, t, sys_event, rc_get_sys_at_x)
-                
-                if (rc_get_sys_at_x == SUCCESS_RC) then
-                    call get_sys_after_impact(status%i_cv(1), sys_event, sys_new, status%rc)
+                i_cv_x_event = status%i_cv(1)
+                if (.not. is_close(sys_old%cv(i_cv_x_event)%x_min%v%v, sys_old%cv(i_cv_x_event)%x%v%v)) then
+                    call get_sys_at_x(t_old, dt, i_cv_x_event, sys_old%cv(i_cv_x_event)%x_min, &
+                                        sys_old, sys_new, t, sys_event, rc_get_sys_at_x)
+                    
+                    if (rc_get_sys_at_x == SUCCESS_RC) then
+                        call get_sys_after_impact(i_cv_x_event, sys_event, sys_new, status%rc)
+                    else
+                        status%rc = rc_get_sys_at_x
+                    end if
                 else
-                    status%rc = rc_get_sys_at_x
+                    ! For this to happen, the time between plunger impacts is now below `dt`.
+                    ! This case will normally stall because the secant method implicitly assumes there is one intersection.
+                    ! It would be easiest to simply refuse to resolve the impacts beyond this point.
+                    ! `t` to `t_old` so that the time iteration is rerun.
+                    ! Set the `cor` to zero so that the plunger stops when the time iteration is rerun.
+                    
+                    status%rc = IMPACT_STOP_OTHER_RUN_RC
+                    t = t_old
+                    sys_new = sys_old
+                    call sys_new%cv(i_cv_x_event)%x_dot%v%init_const(0.0_WP, n_d)
+                    rm_p_eff = sys_new%cv(i_cv_x_event)%rm_p_eff()
+                    if (.not. is_close(rm_p_eff%v%v, 0.0_WP)) then
+                        sys_new%cv(i_cv_x_event)%e_m = sys_new%cv(i_cv_x_event)%e_m &
+                                    + (0.5_WP/rm_p_eff)*square(sys_old%cv(i_cv_x_event)%x_dot)
+                    end if
+                    call sys_new%cv(i_cv_x_event)%rm_p%v%init_const(0.0_WP, n_d)
                 end if
         end select event_case
         
@@ -1800,7 +1822,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
             case (DT_CHANGED_RECOVERY_RUN_RC)
                 write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", CONVERT_S_TO_MS*t%v%v, " ms dt=", &
                         CONVERT_S_TO_MS*dt%v%v, " ms: dt increased"
-            case (IMPACT_STOP_RUN_RC)
+            case (IMPACT_STOP_VELOCITY_RUN_RC, IMPACT_STOP_OTHER_RUN_RC)
                 write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", CONVERT_S_TO_MS*t%v%v, " ms dt=", &
                         CONVERT_S_TO_MS*dt%v%v, " ms: final plunger impact, plunger now immobile"
             case default
@@ -2084,7 +2106,13 @@ pure subroutine get_sys_at_x(t_old, dt, i_cv_x_event, x_event, sys_old, sys_new,
     sys_im2 = sys_old
     sys_im1 = sys_new
     
-    !print *, "start", x_event%v%v, sys_old%cv(i_cv_x_event)%x_dot%v%v
+    call assert(.not. is_close(x_event%v%v, sys_old%cv(i_cv_x_event)%x%v%v), &
+                    "cva (get_sys_at_x): this configuration will stall", &
+                    print_real=[x_event%v%v, sys_old%cv(i_cv_x_event)%x%v%v, &
+                                    sys_new%cv(i_cv_x_event)%x%v%v])
+    
+!    print *, "start", x_event%v%v, sys_old%cv(i_cv_x_event)%x%v%v, sys_old%cv(i_cv_x_event)%x_dot%v%v, &
+!                        (sys_new%cv(i_cv_x_event)%x < x_event)
     rc = MAX_ITERS_GET_SYS_AT_X_RUN_RC
     do i = 1, MAX_ITERS_GET_SYS_AT_X
         ! This is one of the stopping criteria recommended by Wikipedia.
@@ -2119,6 +2147,8 @@ pure subroutine get_sys_at_x(t_old, dt, i_cv_x_event, x_event, sys_old, sys_new,
             exit
         end if
         
+        !print *, dt_i%v%v, sys_i%cv(i_cv_x_event)%x%v%v, sys_i%cv(i_cv_x_event)%x_dot%v%v
+        
         call assert(sys_i%cv(i_cv_x_event)%x >= min_x, &
                         "cva (get_sys_at_x): x_i >= min_x violated", &
                         print_real=[sys_old%cv(i_cv_x_event)%x%v%v, sys_i%cv(i_cv_x_event)%x%v%v, &
@@ -2150,7 +2180,7 @@ pure subroutine get_sys_at_x(t_old, dt, i_cv_x_event, x_event, sys_old, sys_new,
     t = t_old + dt_i
 end subroutine get_sys_at_x
 
-!tripwire$ begin AC1ABD23 Update `\secref{plunger-impact}` of theory.tex.
+!tripwire$ begin F1BA303E Update `\secref{plunger-impact}` of theory.tex.
 pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact, rc)
     ! Set `sys_before_impact` to the instant immediately after plunger impact.
     ! `sys_before_impact` is right before plunger impact occurs (plunger velocity has no changed yet).
@@ -2167,8 +2197,8 @@ pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact, 
                     print_real=[sys_before_impact%cv(i_cv)%cor%v%v], print_integer=[i_cv])
     call assert(sys_before_impact%cv(i_cv)%cor%v%v   <= 1.0_WP, "cva (get_sys_after_impact): cor <= 1 violated", &
                     print_real=[sys_before_impact%cv(i_cv)%cor%v%v], print_integer=[i_cv])
-    call assert(sys_before_impact%cv(i_cv)%x_dot%v%v <= 0.0_WP, &
-                    "cva (get_sys_after_impact): the velocity before impact should be negative or zero, " &
+    call assert(sys_before_impact%cv(i_cv)%x_dot%v%v < 0.0_WP, &
+                    "cva (get_sys_after_impact): the velocity before impact should be negative, " &
                         // "and is assumed so with the IMPACT_STOP_VELOCITY conditional", &
                     print_real=[sys_before_impact%cv(i_cv)%x_dot%v%v], print_integer=[i_cv])
     call assert(rc == X_LT_X_MIN_RUN_RC, "cva (get_sys_after_impact): wrong rc?", print_integer=[i_cv])
@@ -2204,7 +2234,7 @@ pure subroutine get_sys_after_impact(i_cv, sys_before_impact, sys_after_impact, 
         ! Then if the pressure in the CV overcomes the counteracting force, the plunger can start moving again.
         if (is_close(sys_after_impact%cv(i_cv)%x_dot%v%v, 0.0_WP)) then
             call sys_after_impact%cv(i_cv)%rm_p%v%init_const(0.0_WP, size(sys_after_impact%cv(i_cv)%rm_p%v%d))
-            rc = IMPACT_STOP_RUN_RC
+            rc = IMPACT_STOP_VELOCITY_RUN_RC
         end if
     end if
 end subroutine get_sys_after_impact

docs/theory.tex
@@ -412,6 +412,7 @@ Having a non-zero coefficient of restitution will lead to an infinite number of 
 The number of bounces in reality is finite, and the decreasing time between bounces can cause similar numerical issues when the time between bounces becomes comparable to the time step.
 A simple and crude way to fix this problem is to set the velocity of the plunger after impact to zero if the magnitude of the velocity before impact is below a threshold.
 The threshold used in BlasterSim at the moment is $\impactstopvelocity$~cm/s.
+BlasterSim will also set the velocity of the plunger after impact to zero if it detects that the time between plunger impacts has decreased below the time step.
 
 If the plunger velocity after impact is zero (either due to the coefficient of restitution being zero or the velocity threshold mentioned in the previous paragraph), BlasterSim can stall.
 After impact, the plunger is located at the end of the plunger tube.

docs/usage.tex
@@ -191,8 +191,10 @@ BlasterSim will continue to the next time step.
 See \secref{time-integration}.
 BlasterSim will continue to the next time step.
 \textbfwarning{If another event or error occurred during this time step, that return code will override this return code.}
-\item[return code -6:] A plunger has impacted the end of the plunger tube with zero plunger velocity after impact.
+\item[return codes -6 and -7:] A plunger has impacted the end of the plunger tube with zero plunger velocity after impact.
 BlasterSim will continue to the next time step.
+Return code -6 means that the velocity before impact was below the threshold of $\impactstopvelocity$~cm/s where BlasterSim dissipates all remaining plunger kinetic energy to prevent an infinite series of plunger impacts.
+Return code -7 means that the the time between plunger impacts has decreased below the time step, and BlasterSim chooses to not resolve that impact and instead dissipate all remaining plunger kinetic energy.
 \end{description}
 
 \section{Inputs in general}\label{inputs-general}

commit 7272e14fe21fe8cc395a2dc7b735c567536c707a

fix: Made BlasterSim robust to a rare floating point error that can cause stalling with repeated plunger impacts.
Found via fuzz testing.

Fixes: 7c0cd6e8a5e1b3024d57892977930ff0f738c8cd ("fix: Made BlasterSim more robust to repeated plunger impacts.")

src/cva.f90
@@ -1722,7 +1722,8 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     type(cv_system_type), allocatable :: sys_old, sys_new, sys_temp, sys_event
     
     character(len=CL)     :: error_message
-    integer               :: n_d, i, csv_unit, rc_get_sys_at_x, rc_time_step, i_last_dt_change, i_cv_x_event
+    integer               :: n_d, i, csv_unit, rc_get_sys_at_x, rc_time_step, i_last_dt_change, i_cv_x_event, &
+                                prev_rc
     type(si_time)         :: t, t_old, dt
     logical               :: exit_time_loop, stop_at_first_event_
     type(si_inverse_mass) :: rm_p_eff
@@ -1739,6 +1740,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     call t%v%init_const(0.0_WP, n_d)
     i = 0
     i_last_dt_change = -huge(1)
+    prev_rc          = huge(1)
     
     if (config%csv_output) then
         open(newunit=csv_unit, action="write", status="replace", position="rewind", &
@@ -1809,6 +1811,9 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
                     ! `t` to `t_old` so that the time iteration is rerun.
                     ! Set the `cor` to zero so that the plunger stops when the time iteration is rerun.
                     
+                    call assert(prev_rc /= IMPACT_STOP_OTHER_RUN_RC, &
+                                    "cva (run): repeated IMPACT_STOP_OTHER_RUN_RC")
+                    
                     status%rc = IMPACT_STOP_OTHER_RUN_RC
                     t = t_old
                     sys_new = sys_old
@@ -1859,6 +1864,8 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
         call move_alloc(from=sys_old,  to=sys_temp)
         call move_alloc(from=sys_new,  to=sys_old)
         call move_alloc(from=sys_temp, to=sys_new)
+        
+        prev_rc = status%rc
     end do time_loop
     
     call move_alloc(from=sys_new, to=sys_end)
@@ -1866,7 +1873,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     status%t = t
 end subroutine run
 
-!tripwire$ begin 04CA5CFB Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
+!tripwire$ begin B68743B0 Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
 pure subroutine check_sys(config, sys, sys_start, t, status)
     type(run_config_type), intent(in)             :: config
     type(cv_system_type), allocatable, intent(in) :: sys, sys_start
@@ -1900,7 +1907,9 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
         
         ! Check whether `x < x_min`.
         if ((sys%cv(i_cv)%x < sys%cv(i_cv)%x_min) &
-                .and. (sys%cv(i_cv)%eos /= CONST_EOS)) then
+                .and. (sys%cv(i_cv)%eos /= CONST_EOS) &
+                .and. (.not. is_close(sys%cv(i_cv)%x%v%v, sys%cv(i_cv)%x_min%v%v))) then
+            ! The `is_close` part is needed to avoid the problem where round off error makes `x` a bit off.
             status%rc = X_LT_X_MIN_RUN_RC
             allocate(status%i_cv(1))
             status%i_cv(1) = i_cv

test/fuzz/springer_000003048.nml
@@ -1,0 +1,23 @@
+! Hash where bug was found: 44164cbee64c14321e151961f4376f068a93c99a
+! This would cause BlasterSim to stall, repeating lines like:
+! `i=5988 t=35.820628712520396 ms dt=0.10000000000000000E-001 ms: final plunger impact, plunger now immobile`
+&springer
+d_barrel = 19.199862752569995 ! mm
+d_e = 3.1849284785422127 ! mm
+d_plunger = 4.1119604951313899 ! mm
+delta_pre = 82.236797683931798 ! mm
+k = 1597.4460280886444 ! N/m
+l_draw = 235.86331697571183 ! mm
+l_travel = 1007.1520793847399 ! mm
+m_plunger = 251.91021000517895 ! g
+m_proj = 6.5615886559174568 ! g
+m_spring = 290.40058740603268 ! g
+p_fd_plunger = 8086.7872188691581 ! kPa
+p_fd_proj = 5725.6391954977680 ! kPa
+p_fs_plunger = 5086.0760930536626 ! kPa
+p_fs_proj = 4992.4159675628680 ! kPa
+vol_dead = 22.208676844713061 ! mL
+b = 0.76657582445021022 ! 1
+cor = 0.13526555499880211 ! 1
+csv_output = .false. ! 
+/