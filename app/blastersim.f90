btrettel/blastersim

commit 478753d2856a0f419fa73b7e40c5be863fb815cf

feat: Changes to support guided fuzz testing.

app/blastersim.f90
@@ -8,10 +8,10 @@
 program blastersim
 
 use, intrinsic :: iso_fortran_env, only: IOSTAT_END, ERROR_UNIT, OUTPUT_UNIT
-use prec, only: CL
+use prec, only: CL, WP
 use cli, only: get_input_file_name_from_cli
 use io, only: I_BARREL, read_pneumatic_namelist, read_springer_namelist
-use cva, only: run_config_type, cv_system_type, run_status_type, T_STOP_DEFAULT, run, &
+use cva, only: run_config_type, cv_system_type, run_status_type, T_STOP_DEFAULT, run, MAX_ITERS_TIME_LOOP, &
                     SUCCESS_RC, TIMEOUT_RUN_RC, NEGATIVE_CV_M_TOTAL_RUN_RC, NEGATIVE_CV_TEMP_RUN_RC, &
                     MASS_TOLERANCE_RUN_RC, ENERGY_TOLERANCE_RUN_RC, MASS_DERIV_TOLERANCE_RUN_RC, &
                     ENERGY_DERIV_TOLERANCE_RUN_RC, IDEAL_EOS_RUN_RC, MIRROR_X_TOLERANCE_RUN_RC, &
@@ -20,13 +20,17 @@ use cva, only: run_config_type, cv_system_type, run_status_type, T_STOP_DEFAULT,
 use stopcodes, only: EX_OK, EX_USAGE, EX_SOFTWARE
 use rev, only: TAG, REVISION_DATE, MODIFIED
 use checks, only: assert
+use build, only: FUZZ
+use units
 implicit none
 
 character(len=CL)                 :: input_file, extra, modified_string
 type(run_config_type)             :: config
 type(cv_system_type), allocatable :: sys_start, sys_end
-integer                           :: rc
+integer                           :: rc, out_unit
 type(run_status_type)             :: status
+type(si_length)                   :: l_end, l_travel
+real(WP)                          :: f, sum_g
 
 extra = "<http://trettel.us/blastersim/>" // new_line("a") // "Written by Ben Trettel."
 
@@ -65,9 +69,44 @@ end block nml_blk
 
 call run(config, sys_start, sys_end, status)
 
-!tripwire$ begin BE34A827 Update `\secref{return-codes}` of usage.tex.
 call post_run_checks(sys_start, sys_end, rc)
 
+if (FUZZ) then
+    ! Write out data used in feedback-based fuzzing.
+    
+    ! More time steps indicates more opportunities for things to go wrong, so incentivize that.
+    ! Scale it so that it's not huge.
+    f = -real(status%i, WP)/real(MAX_ITERS_TIME_LOOP, WP)
+    
+    if (allocated(status%data)) then
+        call assert(sum(status%data) >= 0.0_WP, &
+                        "blastersim: all elements of status%data should be non-negative to enable fuzz testing")
+        
+        f = f - sum(status%data)
+    end if
+    
+    ! If successful, no constraints are violated.
+    ! If not successful, set a constraint to incentivize the projectile leaving the barrel.
+    ! With purely random testing, the vast majority of cases do not leave the barrel.
+    ! So I want to test more cases that leave the barrel.
+    if (status%rc < SUCCESS_RC) then
+        sum_g = 0.0_WP
+    else
+        l_travel = sys_start%cv(I_BARREL)%x_stop - sys_start%cv(I_BARREL)%x
+        l_end    = sys_end%cv(I_BARREL)%x        - sys_start%cv(I_BARREL)%x
+        sum_g = (l_end%v%v - l_travel%v%v)/l_end%v%v
+        
+        call assert(sum_g >= 0.0_WP, "blastersim: sum_g >= violated")
+        call assert(sum_g <= 1.0_WP, "blastersim: sum_g <= violated")
+    end if
+    
+    open(newunit=out_unit, action="write", status="replace", position="rewind", &
+            file=trim(input_file) // ".out")
+    write(unit=out_unit, fmt="(es24.17, 1x, es24.17)") f, sum_g
+    close(unit=out_unit)
+end if
+
+!tripwire$ begin 36CD5B66 Update `\secref{return-codes}` of usage.tex.
 if (status%rc < SUCCESS_RC) then
     write(unit=OUTPUT_UNIT, fmt="(a)") "SUCCESS!"
     write(unit=OUTPUT_UNIT, fmt="(a, f0.2, a)") "muzzle velocity: ", sys_end%cv(I_BARREL)%x_dot%v%v, " m/s"
@@ -138,9 +177,7 @@ subroutine refer_to_docs()
 end subroutine refer_to_docs
 
 pure subroutine post_run_checks(sys_start, sys_end, rc)
-    use units
     use io, only: I_SOURCE
-    use prec, only: WP
     
     type(cv_system_type), allocatable, intent(in) :: sys_start, sys_end
     integer, intent(in out)                       :: rc

src/cva.f90
@@ -1881,7 +1881,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     status%i = i
 end subroutine run
 
-!tripwire$ begin B68743B0 Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
+!tripwire$ begin 21C871DF Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
 pure subroutine check_sys(config, sys, sys_start, t, status)
     type(run_config_type), intent(in)             :: config
     type(cv_system_type), allocatable, intent(in) :: sys, sys_start
@@ -1892,7 +1892,7 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
     type(si_mass)        :: m_start, m_total_i, m_total_j, rel_m
     type(si_energy)      :: e_start, rel_e
     type(si_temperature) :: temp_i, temp_j
-    type(si_pressure)    :: p_j
+    type(si_pressure)    :: p_j, p_c
     type(unitless)       :: rel_delta
     real(WP)             :: max_abs_m_deriv, max_abs_e_deriv
     type(si_length)      :: x_sum_start, x_sum
@@ -1921,6 +1921,8 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
             status%rc = X_LT_X_MIN_RUN_RC
             allocate(status%i_cv(1))
             status%i_cv(1) = i_cv
+            allocate(status%data(1))
+            status%data(1) = sys%cv(i_cv)%x_min%v%v - sys%cv(i_cv)%x%v%v
             return
         end if
         
@@ -1931,7 +1933,7 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
             allocate(status%i_cv(1))
             status%i_cv(1) = i_cv
             allocate(status%data(1))
-            status%data(1) = sys%cv(i_cv)%x%v%v
+            status%data(1) = -sys%cv(i_cv)%x%v%v
             return
         end if
         
@@ -1940,23 +1942,23 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
         if (m_total_i%v%v < 0.0_WP) then
             status%rc = NEGATIVE_CV_M_TOTAL_RUN_RC
             
-            allocate(status%data(n_cv))
             n_bad_cv = 0
             do j_cv = 1, n_cv
                 m_total_j = sys%cv(j_cv)%m_total()
                 if (m_total_j%v%v < 0.0_WP) n_bad_cv = n_bad_cv + 1
-                status%data(j_cv) = m_total_j%v%v
             end do
             call assert(n_bad_cv >= 1, "cva (check_sys): number of control volumes with negative mass should be >= 1", &
                             print_integer=[n_bad_cv])
             
             allocate(status%i_cv(n_bad_cv))
+            allocate(status%data(n_bad_cv))
             n_bad_cv = 0
             do j_cv = 1, n_cv
                 m_total_j = sys%cv(j_cv)%m_total()
                 if (m_total_j%v%v < 0.0_WP) then
                     n_bad_cv = n_bad_cv + 1
                     status%i_cv(n_bad_cv) = j_cv
+                    status%data(n_bad_cv) = -m_total_j%v%v
                 end if
             end do
             
@@ -1968,24 +1970,24 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
         if (temp_i%v%v <= 0.0_WP) then
             status%rc = NEGATIVE_CV_TEMP_RUN_RC
             
-            allocate(status%data(n_cv))
             n_bad_cv = 0
             do j_cv = 1, n_cv
                 temp_j = sys%cv(j_cv)%temp()
                 if (temp_j%v%v < 0.0_WP) n_bad_cv = n_bad_cv + 1
-                status%data(j_cv) = temp_j%v%v
             end do
             call assert(n_bad_cv >= 1, &
                 "cva (check_sys): number of control volumes with negative or zero temperature should be >= 1", &
                             print_integer=[n_bad_cv])
             
             allocate(status%i_cv(n_bad_cv))
+            allocate(status%data(n_bad_cv))
             n_bad_cv = 0
             do j_cv = 1, n_cv
                 temp_j = sys%cv(j_cv)%temp()
                 if (temp_j%v%v < 0.0_WP) then
                     n_bad_cv = n_bad_cv + 1
                     status%i_cv(n_bad_cv) = j_cv
+                    status%data(n_bad_cv) = -temp_j%v%v
                 end if
             end do
             
@@ -1997,26 +1999,27 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
             if (sys%cv(i_cv)%p() >= sys%cv(i_cv)%p_c()) then
                 status%rc = IDEAL_EOS_RUN_RC
                 
-                allocate(status%data(n_cv))
                 n_bad_cv = 0
                 do j_cv = 1, n_cv
                     if (sys%cv(j_cv)%eos == IDEAL_EOS) then
                         p_j = sys%cv(j_cv)%p()
                         if (p_j >= sys%cv(j_cv)%p_c()) n_bad_cv = n_bad_cv + 1
-                        status%data(j_cv) = p_j%v%v
                     end if
                 end do
                 call assert(n_bad_cv >= 1, "cva (check_sys): number of control volumes with p >= p_c should be >= 1", &
                             print_integer=[n_bad_cv])
                 
                 allocate(status%i_cv(n_bad_cv))
+                allocate(status%data(n_bad_cv))
                 n_bad_cv = 0
                 do j_cv = 1, n_cv
                     if (sys%cv(j_cv)%eos == IDEAL_EOS) then
                         p_j = sys%cv(j_cv)%p()
-                        if (p_j >= sys%cv(j_cv)%p_c()) then
+                        p_c = sys%cv(j_cv)%p_c()
+                        if (p_j >= p_c) then
                             n_bad_cv = n_bad_cv + 1
                             status%i_cv(n_bad_cv) = j_cv
+                            status%data(n_bad_cv) = p_j%v%v - p_c%v%v
                         end if
                     end if
                 end do
@@ -2074,9 +2077,9 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
         end do
         if (max_abs_m_deriv > MASS_DERIV_TOLERANCE) then
             status%rc = MASS_DERIV_TOLERANCE_RUN_RC
-            allocate(status%data(2))
+            allocate(status%data(1))
             status%data(1) = max_abs_m_deriv
-            status%data(2) = real(i_d_max, WP)
+            !status%data(2) = real(i_d_max, WP) ! commented out because it wouldn't work as a fuzz testing objective function
             return
         end if
         
@@ -2101,9 +2104,9 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
         end do
         if (max_abs_e_deriv > ENERGY_DERIV_TOLERANCE) then
             status%rc = ENERGY_DERIV_TOLERANCE_RUN_RC
-            allocate(status%data(2))
+            allocate(status%data(1))
             status%data(1) = max_abs_e_deriv
-            status%data(2) = real(i_d_max, WP)
+            !status%data(2) = real(i_d_max, WP) ! commented out because it wouldn't work as a fuzz testing objective function
             return
         end if
     end if

test/test_cva.f90
@@ -2270,9 +2270,8 @@ subroutine test_check_sys(tests)
     call tests%integer_eq(status%rc, NEGATIVE_CV_M_TOTAL_RUN_RC, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, status%rc")
     call tests%integer_eq(size(status%i_cv), 1, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, size(status%i_cv)")
     call tests%integer_eq(status%i_cv(1), 1, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, status%i_cv(1)")
-    call tests%integer_eq(size(status%data), 2, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, size(status%data)")
-    call tests%real_eq(status%data(1), -0.5_WP, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, status%data(1)")
-    call tests%real_eq(status%data(2), 1.5_WP, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, status%data(2)")
+    call tests%integer_eq(size(status%data), 1, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, size(status%data)")
+    call tests%real_eq(status%data(1), 0.5_WP, "test_check_sys, NEGATIVE_CV_M_TOTAL_RUN_RC, status%data(1)")
     
     ! `NEGATIVE_CV_TEMP_RUN_RC`
     
@@ -2325,10 +2324,8 @@ subroutine test_check_sys(tests)
     call tests%integer_eq(status%rc, NEGATIVE_CV_TEMP_RUN_RC, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, status%rc")
     call tests%integer_eq(size(status%i_cv), 1, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, size(status%i_cv)")
     call tests%integer_eq(status%i_cv(1), 2, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, status%i_cv(1)")
-    call tests%integer_eq(size(status%data), 2, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, size(status%data)")
-    call tests%real_gt(status%data(1), 0.0_WP, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, status%data(1) sign")
-    call tests%real_lt(status%data(2), 0.0_WP, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, status%data(2) sign")
-    call tests%real_eq(status%data(2), -100.0_WP, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, status%data(2)")
+    call tests%integer_eq(size(status%data), 1, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, size(status%data)")
+    call tests%real_eq(status%data(1), 100.0_WP, "test_check_sys, NEGATIVE_CV_TEMP_RUN_RC, status%data(1)")
     
     ! `MASS_TOLERANCE_RUN_RC`
     
@@ -2481,9 +2478,8 @@ subroutine test_check_sys(tests)
     call tests%integer_eq(size(sys%cv(1)%x%v%d), 2, "test_check_sys, MASS_DERIV_TOLERANCE_RUN_RC, n_d")
     call check_sys(config, sys, sys_start, t, status)
     call tests%integer_eq(status%rc, MASS_DERIV_TOLERANCE_RUN_RC, "test_check_sys, MASS_DERIV_TOLERANCE_RUN_RC, status%rc")
-    call tests%integer_eq(size(status%data), 2, "test_check_sys, MASS_DERIV_TOLERANCE_RUN_RC, size(status%data)")
+    call tests%integer_eq(size(status%data), 1, "test_check_sys, MASS_DERIV_TOLERANCE_RUN_RC, size(status%data)")
     call tests%real_eq(status%data(1), 1.0_WP, "test_check_sys, MASS_DERIV_TOLERANCE_RUN_RC, status%data(1)")
-    call tests%real_eq(status%data(2), 1.0_WP, "test_check_sys, MASS_DERIV_TOLERANCE_RUN_RC, status%data(2)")
     
     ! `ENERGY_DERIV_TOLERANCE_RUN_RC`
     
@@ -2534,9 +2530,8 @@ subroutine test_check_sys(tests)
     call tests%integer_eq(size(sys%cv(1)%x%v%d), 2, "test_check_sys, MASS_DERIV_TOLERANCE_RUN_RC, n_d")
     call check_sys(config, sys, sys_start, t, status)
     call tests%integer_eq(status%rc, ENERGY_DERIV_TOLERANCE_RUN_RC, "test_check_sys, ENERGY_DERIV_TOLERANCE_RUN_RC, status%rc")
-    call tests%integer_eq(size(status%data), 2, "test_check_sys, ENERGY_DERIV_TOLERANCE_RUN_RC, size(status%data)")
+    call tests%integer_eq(size(status%data), 1, "test_check_sys, ENERGY_DERIV_TOLERANCE_RUN_RC, size(status%data)")
     call tests%real_eq(status%data(1), 1.0_WP, "test_check_sys, ENERGY_DERIV_TOLERANCE_RUN_RC, status%data(1)")
-    call tests%real_eq(status%data(2), 1.0_WP, "test_check_sys, ENERGY_DERIV_TOLERANCE_RUN_RC, status%data(2)")
     
     ! `IDEAL_EOS_RUN_RC`
     
@@ -2588,11 +2583,10 @@ subroutine test_check_sys(tests)
     call tests%integer_eq(status%rc, IDEAL_EOS_RUN_RC, "test_check_sys, IDEAL_EOS_RUN_RC, status%rc")
     call tests%integer_eq(size(status%i_cv), 1, "test_check_sys, IDEAL_EOS_RUN_RC, size(status%i_cv)")
     call tests%integer_eq(status%i_cv(1), 1, "test_check_sys, IDEAL_EOS_RUN_RC, status%i_cv(1)")
-    call tests%integer_eq(size(status%data), 2, "test_check_sys, IDEAL_EOS_RUN_RC, size(status%data)")
+    call tests%integer_eq(size(status%data), 1, "test_check_sys, IDEAL_EOS_RUN_RC, size(status%data)")
     p = sys%cv(1)%p()
     call tests%real_gt(p%v%v, DRY_AIR%p_c, "test_check_sys, IDEAL_EOS_RUN_RC, p >= p_c")
-    call tests%real_gt(status%data(1), DRY_AIR%p_c, "test_check_sys, IDEAL_EOS_RUN_RC, status%data(1) sign")
-    call tests%real_lt(status%data(2), DRY_AIR%p_c, "test_check_sys, IDEAL_EOS_RUN_RC, status%data(2) sign")
+    call tests%real_gt(status%data(1), 0.0_WP, "test_check_sys, IDEAL_EOS_RUN_RC, status%data(1) sign")
     
     ! `MIRROR_X_TOLERANCE_RUN_RC`
     
@@ -2655,15 +2649,14 @@ subroutine test_check_sys(tests)
     call tests%integer_eq(status%rc, X_LT_X_MIN_RUN_RC, "test_check_sys, X_LT_X_MIN_RUN_RC, status%rc")
     call tests%integer_eq(size(status%i_cv), 1, "test_check_sys, X_LT_X_MIN_RUN_RC, size(status%i_cv)")
     call tests%integer_eq(status%i_cv(1), 2, "test_check_sys, X_LT_X_MIN_RUN_RC, status%i_cv(1)")
+    call tests%integer_eq(size(status%data), 1, "test_check_sys, X_LT_X_MIN_RUN_RC, size(status%data)")
+    call tests%real_eq(status%data(1), 0.9_WP, "test_check_sys, X_LT_X_MIN_RUN_RC, status%data(1)")
     
-    ! TODO: `X_BLOW_UP_RUN_RC`
-    ! TODO: `X_DOT_BLOW_UP_RUN_RC`
-    ! TODO: `M_BLOW_UP_RUN_RC`
-    ! TODO: `E_BLOW_UP_RUN_RC`
-    ! TODO: `E_F_BLOW_UP_RUN_RC`
+    ! TODO: `NEGATIVE_CV_X_RUN_RC`
+    ! TODO: other return codes
 end subroutine test_check_sys
 
-!tripwire$ begin E3F28CD6 Update `\secref{single-cv-exact}` of verval.tex.
+!tripwire$ begin 6DDDD431 Update `\secref{single-cv-exact}` of verval.tex.
 pure function exact_x_dot(sys_0, x)
     use checks, only: assert, is_close
     use cva, only: IDEAL_EOS, CONST_EOS, NORMAL_CV_TYPE, MIRROR_CV_TYPE, cv_system_type
@@ -2780,7 +2773,7 @@ subroutine exact_x_dot_de(n, ne, ne_d)
     
     if (status%rc == ENERGY_DERIV_TOLERANCE_RUN_RC) then
         print *, "ENERGY_DERIV_TOLERANCE_RUN_RC"
-        print *, status%data(1), int(status%data(2)), ENERGY_DERIV_TOLERANCE
+        print *, status%data(1), ENERGY_DERIV_TOLERANCE
     end if
     call assert(status%rc == TIMEOUT_RUN_RC, "test_single_cv_exact, status%rc")
     

commit 793a858eec9ddec1eba8463c84584c31e40ec594

fix: Fixed the fuzz testing constraint.
Fixes: 478753d2856a0f419fa73b7e40c5be863fb815cf ("feat: Changes to support guided fuzz testing.")

app/blastersim.f90
@@ -19,7 +19,7 @@ use cva, only: run_config_type, cv_system_type, run_status_type, T_STOP_DEFAULT,
                     RK_STAGE_NEGATIVE_MASS_RC, RK_STAGE_NEGATIVE_ENERGY_RC, MAX_VELOCITY_EXCEEDED_RC
 use stopcodes, only: EX_OK, EX_USAGE, EX_SOFTWARE
 use rev, only: TAG, REVISION_DATE, MODIFIED
-use checks, only: assert
+use checks, only: assert, is_close
 use build, only: FUZZ
 use units
 implicit none
@@ -94,10 +94,16 @@ if (FUZZ) then
     else
         l_travel = sys_start%cv(I_BARREL)%x_stop - sys_start%cv(I_BARREL)%x
         l_end    = sys_end%cv(I_BARREL)%x        - sys_start%cv(I_BARREL)%x
-        sum_g = (l_end%v%v - l_travel%v%v)/l_end%v%v
+        sum_g    = (l_travel%v%v - l_end%v%v)/l_travel%v%v
         
-        call assert(sum_g >= 0.0_WP, "blastersim: sum_g >= violated")
-        call assert(sum_g <= 1.0_WP, "blastersim: sum_g <= violated")
+        call assert(.not. is_close(l_travel%v%v, 0.0_WP), "blastersim: l_travel must be /= 0")
+        
+        call assert(sum_g >= 0.0_WP, "blastersim: sum_g >= 0 violated", &
+                        print_real=[sum_g, l_end%v%v, l_travel%v%v])
+        
+        ! Commented out as until I fix the friction to not have any backwards motion, `sum_g` can go above 1.
+        !call assert(sum_g <= 1.0_WP, "blastersim: sum_g <= 1 violated", &
+                        !print_real=[sum_g, l_end%v%v, l_travel%v%v])
     end if
     
     open(newunit=out_unit, action="write", status="replace", position="rewind", &

src/geninput_pneumatic.nml
@@ -7,6 +7,7 @@ write_return          = .false.
 executable            = "./blastersim"
 acceptable_exit_codes = 0, 64
 run_time_threshold    = 300.0
+nmlfuzz_mode          = 2
 /
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

src/geninput_springer.nml
@@ -7,6 +7,7 @@ write_return          = .false.
 executable            = "./blastersim"
 acceptable_exit_codes = 0, 64
 run_time_threshold    = 300.0
+nmlfuzz_mode          = 2
 /
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!