btrettel/blastersim

commit 33a937362afc22bf976377b0f2521b990b685955

feat: Started tracking peak pressure in each control volume.
test: For fuzz testing, now there's a constraint on the peak pressure to encourage the projectile to leave the barrel.

app/blastersim.f90
@@ -10,7 +10,7 @@ program blastersim
 use, intrinsic :: iso_fortran_env, only: IOSTAT_END, ERROR_UNIT, OUTPUT_UNIT
 use prec, only: CL, WP
 use cli, only: get_input_file_name_from_cli
-use io, only: I_BARREL, read_pneumatic_namelist, read_springer_namelist
+use io, only: I_BARREL, I_BARREL_ATM, read_pneumatic_namelist, read_springer_namelist
 use cva, only: run_config_type, cv_system_type, run_status_type, T_STOP_DEFAULT, run, MAX_ITERS_TIME_LOOP, &
                     SUCCESS_RC, TIMEOUT_RUN_RC, NEGATIVE_CV_M_TOTAL_RUN_RC, NEGATIVE_CV_TEMP_RUN_RC, &
                     MASS_TOLERANCE_RUN_RC, ENERGY_TOLERANCE_RUN_RC, MASS_DERIV_TOLERANCE_RUN_RC, &
@@ -85,16 +85,23 @@ if (FUZZ) then
         f = f - sum(status%data)
     end if
     
-    ! If successful, no constraints are violated.
-    ! If not successful, set a constraint to incentivize the projectile leaving the barrel.
-    ! With purely random testing, the vast majority of cases do not leave the barrel.
-    ! So I want to test more cases that leave the barrel.
     if (status%rc < SUCCESS_RC) then
+        ! If successful, no constraints are violated.
         sum_g = 0.0_WP
     else
+        ! If not successful, set a constraint to incentivize the projectile leaving the barrel.
+        ! With purely random testing, the vast majority of cases do not leave the barrel.
+        ! So I want to test more cases that leave the barrel.
+        
+        ! One part of the constraint is whether the pressure is high enough to cause the projectile to move at all.
+        sum_g = max(0.0_WP, (sys_end%cv(I_BARREL)%p_fs%v%v &
+                                - (sys_end%cv(I_BARREL)%p_peak%v%v - sys_end%cv(I_BARREL_ATM)%p_const%v%v)) &
+                                    / sys_end%cv(I_BARREL)%p_fs%v%v)
+        
+        ! The other part of the constraint is how far the projectile moves down the barrel.
         l_travel = sys_start%cv(I_BARREL)%x_stop - sys_start%cv(I_BARREL)%x
         l_end    = sys_end%cv(I_BARREL)%x        - sys_start%cv(I_BARREL)%x
-        sum_g    = (l_travel%v%v - l_end%v%v)/l_travel%v%v
+        sum_g    = sum_g + (l_travel%v%v - l_end%v%v)/l_travel%v%v
         
         call assert(.not. is_close(l_travel%v%v, 0.0_WP), "blastersim: l_travel must be /= 0")
         

docs/dev.tex
@@ -151,10 +151,12 @@ I intend to slowly clean up and add these notes to this documentation.
 Note that this is different from convention used for intensive and extensive quantities in the typeset math in this documentation as discussed in \secref{notation}.
 Because each variable in BlasterSim's source code is given a unit (see \secref{unit-checking}), whether a quantity is extensive or intensive can be determined from the units for that quantity, if unclear.
 
-\item The control volume index for the barrel is \texttt{I\_BARREL}, which is set to $\ibarrel$.
+\item The control volume index for the pressurized side of the barrel is \texttt{I\_BARREL}, which is set to $\ibarrel$.
 This convention allows BlasterSim to easily know which control volume is the barrel.
 
 \item The control volume index for the ``source'' (which control volume the propelling gas originates from, whether plunger tube or pressure chamber) is \texttt{I\_SOURCE}, which is set to $\isource$.
+
+\item The control volume index for the atmospheric side of the barrel is \texttt{I\_BARREL\_ATM}, which is set to $\ibarrelatm$.
 
 \item \texttt{integer} variables named \texttt{rc} are used for return codes.
 0 or negative return codes indicate success, with 0 being normal success and negative numbers having a special context-dependent meaning.
@@ -175,7 +177,7 @@ This general control volume view has consistent notation for all control volumes
 A consequence of this is that plunger velocities will be negative as the plunger volume decreases, not positive as some might expect.
 Projectile velocities are positive as the projectile moves towards the barrel exit, as expected.
 
-\item In cva.f90, in the \texttt{cv\_type} derived type, there is a member variable named \texttt{i\_cv\_mirror}
+\item In cva.f90, in the \texttt{cv\_type} derived type, there is a member variable named \texttt{i\_cv\_mirror}.
 For control volumes with a projectile/plunger, \texttt{i\_cv\_mirror} is set to the control volume index for the control volume on the other side of the projectile/plunger.
 For constant volume chambers where there is no projectile/plunger, set \texttt{i\_cv\_mirror} to zero to disable the mirror control volume feature.
 

src/cva.f90
@@ -113,6 +113,7 @@ type, public :: cv_type ! control volume
     type(si_energy)            :: e_g    ! energy of gas in control volume
     type(si_energy)            :: e_f    ! energy lost to projectile/plunger friction in control volume
     type(si_energy)            :: e_m    ! energy lost to plunger impact in control volume
+    type(si_pressure)          :: p_peak ! peak pressure so far in this control volume
     
     ! constants
     character(len=32)           :: label           ! human-readable label for control volume
@@ -728,6 +729,7 @@ pure subroutine set(cv, x, x_dot, y, p, temp_atm, label, csa, rm_p, p_fs, p_fd, 
     cv%delta_pre   = delta_pre
     cv%gas         = gas
     cv%i_cv_mirror = i_cv_mirror
+    cv%p_peak      = p
     
     if (present(x_stop)) then
         cv%x_stop = x_stop
@@ -928,6 +930,7 @@ pure subroutine set_const(cv, label, csa, p_const, temp_const, gas, y_const, i_c
     call assert(cv%x%v%v < cv%x_stop%v%v, "cva (set_const): x >= x_stop will cause immediate termination of run", &
                     print_real=[cv%x%v%v, cv%x_stop%v%v])
     
+    cv%p_peak      = p_const
     cv%label       = label
     cv%csa         = csa
     cv%eos         = CONST_EOS
@@ -1600,6 +1603,9 @@ pure subroutine calculate_next_time_step(sys_old, t, dt, sys_new, rc)
         end do
         
         call assert_mass(sys_new%cv(i_cv), "calculate_next_time_step")
+        
+        sys_new%cv(i_cv)%p_peak = max(sys_new%cv(i_cv)%p_peak, sys_new%cv(i_cv)%p())
+        call assert(sys_new%cv(i_cv)%p_peak%v%v >= 0.0_WP, "cva (calculate_next_time_step): ")
     end do
 end subroutine calculate_next_time_step
 

src/io.f90
@@ -12,8 +12,9 @@ use units
 use checks, only: assert
 implicit none
 
-integer, parameter, public :: I_BARREL = 1
-integer, parameter, public :: I_SOURCE = 2
+integer, parameter, public :: I_BARREL     = 1
+integer, parameter, public :: I_SOURCE     = 2
+integer, parameter, public :: I_BARREL_ATM = 3
 
 public :: write_latex_engineering
 public :: create_barrel
@@ -197,7 +198,7 @@ subroutine read_springer_namelist(input_file, sys, config, rc_read, actual_v_muz
     type(si_area)      :: csa_plunger, csa_barrel
     type(si_length)    :: x_dead_plunger
     
-    integer, parameter :: I_PLUNGER = I_SOURCE, I_BARREL_ATM  = 3, I_PLUNGER_ATM = 4
+    integer, parameter :: I_PLUNGER = I_SOURCE, I_PLUNGER_ATM = 4
     
     !tripwire$ begin 2807623D Update \secref{notation} of theory.tex.
     type(gas_type), parameter :: BARREL_GAS(*)   = [DRY_AIR], &

test/test_cva.f90
@@ -75,7 +75,7 @@ subroutine write_defaults()
                     DT_BACKOFF_IMPACT, DT_RECOVERY_ITERATIONS, DT_RECOVERY, PRINT_FREQUENCY, IMPACT_STOP_VELOCITY
     use gasdata, only: P_ATM, TEMP_ATM, TEMP_0
     use convert, only: CONVERT_C_TO_K, CONVERT_PA_TO_KPA, CONVERT_M_TO_CM
-    use io, only: I_BARREL, I_SOURCE, write_latex_engineering
+    use io, only: I_BARREL, I_SOURCE, I_BARREL_ATM, write_latex_engineering
     use stopcodes, only: EX_USAGE, EX_SOFTWARE
     
     integer :: tex_unit
@@ -109,6 +109,7 @@ subroutine write_defaults()
                                                 CONVERT_M_TO_CM*IMPACT_STOP_VELOCITY, "} % cm/s"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\ibarrel}{", I_BARREL, "}"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\isource}{", I_SOURCE, "}"
+    write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\ibarrelatm}{", I_BARREL_ATM, "}"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\exusage}{", EX_USAGE, "}"
     write(unit=tex_unit, fmt="(a, i0, a)") "\newcommand*{\exsoftware}{", EX_SOFTWARE, "}"
     close(tex_unit)

commit 55e3f9574a6f373665796074154498a8839069b6

fix: Minor fix that should help projectiles leave the barrel in fuzz testing. Fixes: 33a937362afc22bf976377b0f2521b990b685955 ("feat: Started tracking peak pressure in each control volume.")

app/blastersim.f90
@@ -94,9 +94,9 @@ if (FUZZ) then
         ! So I want to test more cases that leave the barrel.
         
         ! One part of the constraint is whether the pressure is high enough to cause the projectile to move at all.
-        sum_g = max(0.0_WP, (sys_end%cv(I_BARREL)%p_fs%v%v &
+        sum_g = max(0.0_WP, (max(sys_end%cv(I_BARREL)%p_fs%v%v, sys_end%cv(I_BARREL)%p_fd%v%v) &
                                 - (sys_end%cv(I_BARREL)%p_peak%v%v - sys_end%cv(I_BARREL_ATM)%p_const%v%v)) &
-                                    / sys_end%cv(I_BARREL)%p_fs%v%v)
+                                    / max(sys_end%cv(I_BARREL)%p_fs%v%v, sys_end%cv(I_BARREL)%p_fd%v%v))
         
         ! The other part of the constraint is how far the projectile moves down the barrel.
         l_travel = sys_start%cv(I_BARREL)%x_stop - sys_start%cv(I_BARREL)%x