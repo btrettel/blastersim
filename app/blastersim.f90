btrettel/blastersim

commit 26e7f525fb3ee2a52435d08c489b7bf478ce9d8c

feat: Made the energy efficiency calculation a subroutine.
test: Incentivizing nmlfuzz to try to get impossible energy efficiencies.

docs: Started documenting energy efficiency.

app/blastersim.f90
@@ -10,7 +10,9 @@ program blastersim
 use, intrinsic :: iso_fortran_env, only: IOSTAT_END, ERROR_UNIT, OUTPUT_UNIT
 use prec, only: CL, WP
 use cli, only: get_input_file_name_from_cli
-use io, only: I_BARREL, I_SOURCE, I_BARREL_ATM, read_pneumatic_namelist, read_springer_namelist
+use io, only: I_BARREL, I_BARREL_ATM, &
+                PNEUMATIC_MODE, SPRINGER_MODE, &
+                read_pneumatic_namelist, read_springer_namelist, calculate_eta
 use cva, only: run_config_type, cv_system_type, run_status_type, T_STOP_DEFAULT, run, MAX_ITERS_TIME_LOOP, &
                     SUCCESS_RC, TIMEOUT_RUN_RC, NEGATIVE_CV_M_TOTAL_RUN_RC, NEGATIVE_CV_TEMP_RUN_RC, &
                     MASS_TOLERANCE_RUN_RC, ENERGY_TOLERANCE_RUN_RC, MASS_DERIV_TOLERANCE_RUN_RC, &
@@ -24,20 +26,15 @@ use build, only: FUZZ
 use units
 implicit none
 
-integer, parameter :: PNEUMATIC_MODE = 1
-integer, parameter :: SPRINGER_MODE  = 2
-
-character(len=CL)                 :: input_file, extra, modified_string
-type(run_config_type)             :: config
+character(len=CL)     :: input_file, extra, modified_string
+type(run_config_type) :: config
+integer               :: rc, mode, out_unit, i_d, len_d_labels
+type(run_status_type) :: status
+type(si_length)       :: l_end, l_travel
+real(WP)              :: f, sum_g
+character(len=2)      :: fmt_part
+type(unitless)        :: eta
 type(cv_system_type), allocatable :: sys_start, sys_end
-integer                           :: rc, mode, out_unit, i_d, len_d_labels
-type(run_status_type)             :: status
-type(si_length)                   :: l_end, l_travel
-real(WP)                          :: f, sum_g
-character(len=2)                  :: fmt_part
-type(si_energy)                   :: input_energy, muzzle_energy
-type(unitless), allocatable       :: y(:)
-type(unitless)                    :: p_0s, eta
 
 extra = "<http://trettel.us/blastersim/>" // new_line("a") // "Written by Ben Trettel."
 
@@ -80,7 +77,9 @@ call run(config, sys_start, sys_end, status)
 
 call post_run_checks(sys_start, sys_end, rc)
 
-!tripwire$ begin 216FEB39 Update `if (rc_read /= 0) then` sections of io.f90 to account for different total `sum_g` here.
+call calculate_eta(sys_start, sys_end, mode, eta)
+
+!tripwire$ begin B7115CD8 Update `if (rc_read /= 0) then` sections of io.f90 to account for different total `sum_g` here.
 ! `sum_g` there needs to strictly be higher than `sum_g` here to encourage going through input validation.
 if (FUZZ) then
     ! Write out data used in feedback-based fuzzing.
@@ -88,6 +87,9 @@ if (FUZZ) then
     ! More time steps indicates more opportunities for things to go wrong, so incentivize that.
     ! Scale it so that it's not huge.
     f = -real(status%i, WP)/real(MAX_ITERS_TIME_LOOP, WP)
+    
+    ! Incentivize impossible efficiencies (`eta < 0` and `eta > 1`).
+    f = f - 4.0_WP*(eta%v%v - 0.5_WP)**2
     
     if (allocated(status%data)) then
         call assert(sum(status%data) >= 0.0_WP, &
@@ -133,7 +135,7 @@ if (FUZZ) then
 end if
 !tripwire$ end
 
-!tripwire$ begin 4F857B96 Update `\secref{return-codes}` of usage.tex.
+!tripwire$ begin DBA7F79F Update `\secref{return-codes}` of usage.tex.
 if (status%rc < SUCCESS_RC) then
     len_d_labels = 0
     do i_d = 1, size(sys_end%cv(I_BARREL)%x_dot%v%d)
@@ -157,26 +159,6 @@ if (status%rc < SUCCESS_RC) then
                     sys_end%cv(I_BARREL)%x_dot%v%d(i_d), " (m/s)/(", trim(config%d_units(i_d)), ")"
     end do
     
-    select case (mode)
-        case (PNEUMATIC_MODE)
-            y    = sys_start%cv(I_BARREL_ATM)%y_const
-            p_0s = sys_start%cv(I_SOURCE)%p() / sys_start%cv(I_BARREL_ATM)%p_const
-            input_energy = (sys_start%cv(I_BARREL_ATM)%p_const * sys_start%cv(I_SOURCE)%vol() &
-                                / (sys_start%cv(I_SOURCE)%gamma(y) - 1.0_WP)) &
-                                    * (p_0s - p_0s**(1.0_WP/sys_start%cv(I_SOURCE)%gamma(y)))
-        case (SPRINGER_MODE)
-            input_energy = 0.5_WP*sys_start%cv(I_SOURCE)%k &
-                                    *(square(sys_start%cv(I_SOURCE)%x - sys_start%cv(I_SOURCE)%x_min &
-                                                + sys_start%cv(I_SOURCE)%delta_pre) &
-                                            - square(sys_start%cv(I_SOURCE)%delta_pre))
-        case default
-            write(unit=ERROR_UNIT, fmt="(a, i0)") "Invalid mode: ", mode
-            stop EX_SOFTWARE, quiet=.true.
-    end select
-    
-    muzzle_energy = sys_end%cv(I_BARREL)%e_k()
-    eta = muzzle_energy / input_energy
-    
     write(unit=OUTPUT_UNIT, fmt="(a" // trim(fmt_part) // ", f9.3, a)") "eta:", 100.0_WP*eta%v%v, " %"
     do i_d = 1, size(eta%v%d)
         ! I tried using unicode partial derivative symbols, but LaTeX returned an error.
@@ -184,9 +166,6 @@ if (status%rc < SUCCESS_RC) then
                     "d(eta)/d(" // trim(config%d_labels(i_d)) // "):", &
                     100.0_WP*eta%v%d(i_d), " (%)/(", trim(config%d_units(i_d)), ")"
     end do
-    
-    call assert(eta%v%v >= 0.0_WP, "blastersim: eta >= 0 violated")
-    call assert(eta%v%v <= 1.0_WP, "blastersim: eta <= 1 violated")
     
     stop EX_OK, quiet=.true.
 else

docs/theory.tex
@@ -426,6 +426,23 @@ The difference with the force is that it might be possible for the force to be o
 Making the plunger immobile prevents that real motion, but is a reasonable approximation for springers.
 It would not be a reasonable approximation when simulating the full cycle of a HPA blaster using an air cylinder.
 
+\subsection{Energy efficiency}\label{energy-efficiency}
+
+The energy efficiency of the interior ballistic process is defined as the fraction of the input energy that is converted into projectile kinetic energy at the muzzle:
+\begin{equation}
+   \eta = \frac{E_\text{muzzle}}{E_\text{input}},
+\end{equation}
+where
+$E_\text{muzzle} = \frac{1}{2} m_\text{proj} V_\text{muzzle}^2$ is the muzzle kinetic energy,
+$E_\text{input}$ is the input energy (to be defined mathematically), 
+$m_\text{proj}$ is the projectile mass, and
+$V_\text{muzzle}$ is the muzzle velocity.
+
+Here, I focus on the efficiency of the interior ballistic process.
+Energy losses due to heat transfer before the trigger is pulled, hysteresis, pump friction in pneumatics, etc., are neglected.
+
+% TODO
+
 %\section{Exterior ballistics}\label{exterior-ballistics}
 % TODO later
 

mk/depends.mk
@@ -22,7 +22,7 @@ src$(DIR_SEP)ga.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)prec.$(OBJ
 
 src$(DIR_SEP)gasdata.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)gasdata.f90
 
-src$(DIR_SEP)io.$(OBJEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 src$(DIR_SEP)io.f90
+src$(DIR_SEP)io.$(OBJEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 src$(DIR_SEP)io.f90
 
 src$(DIR_SEP)ooa.$(OBJEXT): src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)ooa.f90
 
@@ -69,14 +69,14 @@ test_gasdata$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJ
 gasdata.nml: test_gasdata$(BINEXT)
 	$(RUN)test_gasdata$(BINEXT)
 
-test_io$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 test$(DIR_SEP)test_io.f90
-	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) test$(DIR_SEP)test_io.f90
+test_io$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 test$(DIR_SEP)test_io.f90
+	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) test$(DIR_SEP)test_io.f90
 
 io.nml: test_io$(BINEXT)
 	$(RUN)test_io$(BINEXT)
 
-test_validation$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)validation.$(OBJEXT) src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 test$(DIR_SEP)test_validation.f90
-	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)validation.$(OBJEXT) test$(DIR_SEP)test_validation.f90
+test_validation$(BINEXT): src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)validation.$(OBJEXT) src$(DIR_SEP)geninput_pneumatic.f90 src$(DIR_SEP)geninput_springer.f90 test$(DIR_SEP)test_validation.f90
+	$(FC) $(OFLAG) $@ $(FFLAGS) src$(DIR_SEP)$(BUILD).$(OBJEXT) src$(DIR_SEP)checks.$(OBJEXT) src$(DIR_SEP)convert.$(OBJEXT) src$(DIR_SEP)cva.$(OBJEXT) src$(DIR_SEP)fmad.$(OBJEXT) src$(DIR_SEP)ga.$(OBJEXT) src$(DIR_SEP)gasdata.$(OBJEXT) src$(DIR_SEP)io.$(OBJEXT) src$(DIR_SEP)port.$(OBJEXT) src$(DIR_SEP)prec.$(OBJEXT) src$(DIR_SEP)purerng.$(OBJEXT) src$(DIR_SEP)stopcodes.$(OBJEXT) src$(DIR_SEP)timer.$(OBJEXT) src$(DIR_SEP)units.$(OBJEXT) src$(DIR_SEP)unittest.$(OBJEXT) src$(DIR_SEP)validation.$(OBJEXT) test$(DIR_SEP)test_validation.f90
 
 validation.nml: test_validation$(BINEXT)
 	$(RUN)test_validation$(BINEXT)

src/io.f90
@@ -16,6 +16,9 @@ integer, parameter, public :: I_BARREL     = 1
 integer, parameter, public :: I_SOURCE     = 2
 integer, parameter, public :: I_BARREL_ATM = 3
 
+integer, parameter, public :: PNEUMATIC_MODE = 1
+integer, parameter, public :: SPRINGER_MODE  = 2
+
 public :: write_latex_engineering
 public :: create_barrel
 public :: read_springer_namelist, read_pneumatic_namelist
@@ -318,5 +321,40 @@ subroutine read_springer_namelist(input_file, sys, config, rc_read, actual_v_muz
     if (present(actual_v_muzzle_n_))     actual_v_muzzle_n_     = actual_v_muzzle_n
     if (present(actual_rc_))             actual_rc_             = actual_rc
 end subroutine read_springer_namelist
+
+pure subroutine calculate_eta(sys_start, sys_end, mode, eta)
+    use, intrinsic :: iso_fortran_env, only: ERROR_UNIT
+    use cva, only: cv_system_type
+    
+    type(cv_system_type), allocatable, intent(in) :: sys_start, sys_end
+    integer, intent(in)                           :: mode
+    type(unitless), intent(out)                   :: eta
+    
+    type(si_energy) :: input_energy, muzzle_energy
+    type(unitless)  :: p_0s
+    type(unitless), allocatable :: y(:)
+    
+    select case (mode)
+        case (PNEUMATIC_MODE)
+            y    = sys_start%cv(I_BARREL_ATM)%y_const
+            p_0s = sys_start%cv(I_SOURCE)%p() / sys_start%cv(I_BARREL_ATM)%p_const
+            input_energy = (sys_start%cv(I_BARREL_ATM)%p_const * sys_start%cv(I_SOURCE)%vol() &
+                                / (sys_start%cv(I_SOURCE)%gamma(y) - 1.0_WP)) &
+                                    * (p_0s - p_0s**(1.0_WP/sys_start%cv(I_SOURCE)%gamma(y)))
+        case (SPRINGER_MODE)
+            input_energy = 0.5_WP*sys_start%cv(I_SOURCE)%k &
+                                    *(square(sys_start%cv(I_SOURCE)%x - sys_start%cv(I_SOURCE)%x_min &
+                                                + sys_start%cv(I_SOURCE)%delta_pre) &
+                                            - square(sys_start%cv(I_SOURCE)%delta_pre))
+        case default
+            error stop "cva (calculate_eta): invalid mode"
+    end select
+    
+    muzzle_energy = sys_end%cv(I_BARREL)%e_k()
+    eta = muzzle_energy / input_energy
+    
+    call assert(eta%v%v >= 0.0_WP, "blastersim: eta >= 0 violated")
+    call assert(eta%v%v <= 1.0_WP, "blastersim: eta <= 1 violated")
+end subroutine calculate_eta
 
 end module io

commit 1870fa6941a74ed86b3d0b6f211122193d7647fc

fix: eta now won't be calculated unless BlasterSim is successful.
This avoids a segfault.

Fixes: 26e7f525fb3ee2a52435d08c489b7bf478ce9d8c ("feat: Made the energy efficiency calculation a subroutine.")

app/blastersim.f90
@@ -77,9 +77,9 @@ call run(config, sys_start, sys_end, status)
 
 call post_run_checks(sys_start, sys_end, rc)
 
-call calculate_eta(sys_start, sys_end, mode, eta)
+if (status%rc < SUCCESS_RC) call calculate_eta(sys_start, sys_end, mode, eta)
 
-!tripwire$ begin B7115CD8 Update `if (rc_read /= 0) then` sections of io.f90 to account for different total `sum_g` here.
+!tripwire$ begin 9BA1EE8D Update `if (rc_read /= 0) then` sections of io.f90 to account for different total `sum_g` here.
 ! `sum_g` there needs to strictly be higher than `sum_g` here to encourage going through input validation.
 if (FUZZ) then
     ! Write out data used in feedback-based fuzzing.
@@ -87,9 +87,6 @@ if (FUZZ) then
     ! More time steps indicates more opportunities for things to go wrong, so incentivize that.
     ! Scale it so that it's not huge.
     f = -real(status%i, WP)/real(MAX_ITERS_TIME_LOOP, WP)
-    
-    ! Incentivize impossible efficiencies (`eta < 0` and `eta > 1`).
-    f = f - 4.0_WP*(eta%v%v - 0.5_WP)**2
     
     if (allocated(status%data)) then
         call assert(sum(status%data) >= 0.0_WP, &
@@ -102,7 +99,8 @@ if (FUZZ) then
         ! If successful, no constraints are violated.
         sum_g = 0.0_WP
         
-        ! TODO: incentivize `eta` going near 0 or 1
+        ! Incentivize impossible efficiencies (`eta < 0` and `eta > 1`).
+        f = f - 4.0_WP*(eta%v%v - 0.5_WP)**2
     else
         ! If not successful, set a constraint to incentivize the projectile leaving the barrel.
         ! With purely random testing, the vast majority of cases do not leave the barrel.

src/cva.f90
@@ -1894,6 +1894,11 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
                 ! I want something where it's highly unlikely that two time steps of this size in a row will end up here again.
                 ! That might cause an infinite loop.
                 
+                call assert((status%rc == X_EQ_X_STOP_RUN_RC) .and. (prev_rc /= X_EQ_X_STOP_RUN_RC), &
+                                    "cva (run): repeated X_EQ_X_STOP_RUN_RC")
+                call assert((status%rc == X_EQ_X_MIN_RUN_RC) .and. (prev_rc /= X_EQ_X_MIN_RUN_RC), &
+                                    "cva (run): repeated X_EQ_X_MIN_RUN_RC")
+                
                 t       = t_old
                 dt      = 0.9_WP*dt
                 sys_new = sys_old
@@ -1950,7 +1955,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     status%i = i
 end subroutine run
 
-!tripwire$ begin 28486845 Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
+!tripwire$ begin F94B3733 Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
 pure subroutine check_sys(config, sys, sys_start, t, status)
     type(run_config_type), intent(in)             :: config
     type(cv_system_type), allocatable, intent(in) :: sys, sys_start