btrettel/blastersim

commit 56bb154dfff08fa9115f405b748599ccc4fa0b09

fix: Addressing corner cases where get_sys_at_x won't be run.
If the time step lands exactly on x_stop, then previously there would be no sensitivity to l_travel as it won't go through the get_sys_at_x loop. Reproducing that in practice would be difficult aside from a controlled test, which I did not try.

src/cva.f90
@@ -55,13 +55,15 @@ integer, public, parameter :: NORMAL_CV_TYPE = 1
 integer, public, parameter :: MIRROR_CV_TYPE = 2
 integer, public, parameter :: MAX_CV_TYPE    = 2
 
-!tripwire$ begin D22EA909 Update \secref{return-codes} and `actual_rc` in geninput_*.nml.
+!tripwire$ begin 5A3E231B Update \secref{return-codes} and `actual_rc` in geninput_*.nml.
+integer, public, parameter :: X_EQ_X_MIN_RUN_RC              = -9
+integer, public, parameter :: X_EQ_X_STOP_RUN_RC             = -8
 integer, public, parameter :: IMPACT_STOP_OTHER_RUN_RC       = -7
 integer, public, parameter :: IMPACT_STOP_VELOCITY_RUN_RC    = -6
 integer, public, parameter :: DT_CHANGED_RECOVERY_RUN_RC     = -5
 integer, public, parameter :: DT_CHANGED_CONSERVATION_RUN_RC = -4
 integer, public, parameter :: X_LT_X_MIN_RUN_RC              = -3
-integer, public, parameter :: X_GE_X_STOP_RUN_RC             = -2
+integer, public, parameter :: X_GT_X_STOP_RUN_RC             = -2
 integer, public, parameter :: CONTINUE_RUN_RC                = -1
 integer, public, parameter :: SUCCESS_RC                     = 0 ! used for more than just the `run` subroutine.
 integer, public, parameter :: TIMEOUT_RUN_RC                 = 1
@@ -1810,7 +1812,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
         call check_sys(config, sys_new, sys_start, t, status)
         
         event_case: select case (status%rc)
-            case (X_GE_X_STOP_RUN_RC) ! -2
+            case (X_GT_X_STOP_RUN_RC) ! -2
                 ! Use Hénon's trick to ensure that `x == x_stop`.
                 call get_sys_at_x(t_old, dt, status%i_cv(1), sys_old%cv(status%i_cv(1))%x_stop, &
                                         sys_old, sys_new, t, sys_end, rc_get_sys_at_x)
@@ -1823,7 +1825,10 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
                 if (rc_get_sys_at_x == SUCCESS_RC) then
                     call check_sys(config, sys_new, sys_start, t, status)
                     
-                    call assert(status%rc == X_GE_X_STOP_RUN_RC, "cva (run): status%rc == X_GE_X_STOP_RUN_RC violated")
+                    call assert(status%rc == X_EQ_X_STOP_RUN_RC, "cva (run): status%rc == X_EQ_X_STOP_RUN_RC violated")
+                    
+                    ! Set `status%rc` back to `X_GT_X_STOP_RUN_RC` to be able to distinguish between this and `X_EQ_X_STOP_RUN_RC`.
+                    status%rc = X_GT_X_STOP_RUN_RC
                 else
                     status%rc = rc_get_sys_at_x
                 end if
@@ -1859,6 +1864,19 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
                     end if
                     call sys_new%cv(i_cv_x_event)%rm_p%v%init_const(0.0_WP, n_d)
                 end if
+            case (X_EQ_X_STOP_RUN_RC, X_EQ_X_MIN_RUN_RC)
+                ! `x` landed exactly on `x_stop` or `x_min`.
+                ! This sounds great at first because it would avoid needing to run `get_sys_at_x`.
+                ! But running `get_sys_at_x` is necessary to get the proper derivative with respect to `l_travel` and `vol_dead`.
+                ! Rerun this time step so that `get_sys_at_x` will be run.
+                
+                ! 0.9 below is somewhat arbitrary.
+                ! I want something where it's highly unlikely that two time steps of this size in a row will end up here again.
+                ! That might cause an infinite loop.
+                
+                t       = t_old
+                dt      = 0.9_WP*dt
+                sys_new = sys_old
         end select event_case
         
         if (i >= MAX_ITERS_TIME_LOOP) status%rc = MAX_ITERS_TIME_LOOP_RUN_RC
@@ -1885,6 +1903,10 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
             case (IMPACT_STOP_VELOCITY_RUN_RC, IMPACT_STOP_OTHER_RUN_RC)
                 write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", CONVERT_S_TO_MS*t%v%v, " ms dt=", &
                         CONVERT_S_TO_MS*dt%v%v, " ms: final plunger impact, plunger now immobile"
+            case (X_EQ_X_STOP_RUN_RC, X_EQ_X_MIN_RUN_RC)
+                write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", CONVERT_S_TO_MS*t%v%v, " ms dt=", &
+                        CONVERT_S_TO_MS*dt%v%v, " ms: projectile or plunger exactly hit x_stop or x_min, " &
+                        // "redoing this time iteration with smaller time step to get derivatives right"
             case default
                 if ((mod(i, PRINT_FREQUENCY) == 0) .and. (.not. config%const_dt)) &
                         write(unit=*, fmt="(a, i0, a, g0, a, g0, a)") "i=", i, " t=", &
@@ -1908,7 +1930,7 @@ subroutine run(config, sys_start, sys_end, status, stop_at_first_event)
     status%i = i
 end subroutine run
 
-!tripwire$ begin FB0C7DDB Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
+!tripwire$ begin 28486845 Update `\secref{run-time-checks}` and `actual_rc` in geninput_*.nml.
 pure subroutine check_sys(config, sys, sys_start, t, status)
     type(run_config_type), intent(in)             :: config
     type(cv_system_type), allocatable, intent(in) :: sys, sys_start
@@ -1933,9 +1955,16 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
     
     do i_cv = 1, n_cv
         ! Check whether the projectile left the barrel.
-        if ((sys%cv(i_cv)%x >= sys%cv(i_cv)%x_stop) &
-                .or. (is_close(sys%cv(i_cv)%x%v%v, sys%cv(i_cv)%x_stop%v%v))) then
-            status%rc = X_GE_X_STOP_RUN_RC
+        if (sys%cv(i_cv)%x > sys%cv(i_cv)%x_stop) then
+            status%rc = X_GT_X_STOP_RUN_RC
+            allocate(status%i_cv(1))
+            status%i_cv(1) = i_cv
+            return
+        end if
+        
+        ! Check whether the projectile is at `x_stop`, which requires special treatment to get the `l_travel` derivative right.
+        if (is_close(sys%cv(i_cv)%x%v%v, sys%cv(i_cv)%x_stop%v%v)) then
+            status%rc = X_EQ_X_STOP_RUN_RC
             allocate(status%i_cv(1))
             status%i_cv(1) = i_cv
             return
@@ -1947,6 +1976,17 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
                 .and. (.not. is_close(sys%cv(i_cv)%x%v%v, sys%cv(i_cv)%x_min%v%v))) then
             ! The `is_close` part is needed to avoid the problem where round off error makes `x` a bit off.
             status%rc = X_LT_X_MIN_RUN_RC
+            allocate(status%i_cv(1))
+            status%i_cv(1) = i_cv
+            allocate(status%data(1))
+            status%data(1) = sys%cv(i_cv)%x_min%v%v - sys%cv(i_cv)%x%v%v
+            return
+        end if
+        
+        ! Check whether the plunger is at `x_min`, which requires special treatment to get the `l_travel` derivative right.
+        if ((is_close(sys%cv(i_cv)%x%v%v, sys%cv(i_cv)%x_min%v%v)) &
+                .and. (sys%cv(i_cv)%eos /= CONST_EOS)) then
+            status%rc = X_EQ_X_MIN_RUN_RC
             allocate(status%i_cv(1))
             status%i_cv(1) = i_cv
             allocate(status%data(1))

docs/usage.tex
@@ -198,6 +198,12 @@ BlasterSim will continue to the next time step.
 BlasterSim will continue to the next time step.
 Return code -6 means that the velocity before impact was below the threshold of $\impactstopvelocity$~cm/s where BlasterSim dissipates all remaining plunger kinetic energy to prevent an infinite series of plunger impacts.
 Return code -7 means that the the time between plunger impacts has decreased below the time step, and BlasterSim chooses to not resolve that impact and instead dissipate all remaining plunger kinetic energy.
+\item[return code -8 and -9:] These return codes cover corner cases where a projectile/plunger hit a termination or impact location without going running the \texttt{get\_sys\_at\_x} subroutine.
+A user is highly unlikely to observe these return codes.
+Running the \texttt{get\_sys\_at\_x} subroutine at these events is necessary for automatic differentiation to work properly.
+In these return codes, the time iteration will return to the previous time iteration with a slightly reduced time step so that the \texttt{get\_sys\_at\_x} subroutine will be run.
+Return code -8 means that a projectile/plunger has exactly hit its termination position.
+Return code -9 means that a projectile/plunger has exactly hit its minimum position.
 \end{description}
 
 \section{Inputs in general}\label{inputs-general}

test/test_cva.f90
@@ -1629,7 +1629,7 @@ subroutine test_conservation_1(tests)
     use convert
     use gasdata, only: DRY_AIR
     use prec, only: PI
-    use cva, only: MIRROR_CV_TYPE, X_GE_X_STOP_RUN_RC, cv_system_type, run_config_type, run_status_type, &
+    use cva, only: MIRROR_CV_TYPE, X_GT_X_STOP_RUN_RC, cv_system_type, run_config_type, run_status_type, &
                     run
     
     type(test_results_type), intent(in out) :: tests
@@ -1741,9 +1741,9 @@ subroutine test_conservation_1(tests)
     call config%set("test_conservation", 1, csv_output=.true., csv_frequency=100, d_labels=d_labels, d_units=d_units)
     call run(config, sys_start, sys_end, status)
     
-    call tests%integer_eq(status%rc, X_GE_X_STOP_RUN_RC, "test_conservation_1, status%rc")
+    call tests%integer_eq(status%rc, X_GT_X_STOP_RUN_RC, "test_conservation_1, status%rc")
     
-    if (status%rc /= X_GE_X_STOP_RUN_RC) then
+    if (status%rc /= X_GT_X_STOP_RUN_RC) then
         if (allocated(status%data)) print *, status%data(1)
         if (allocated(status%i_cv)) print *, status%i_cv(1)
     end if
@@ -2039,7 +2039,7 @@ subroutine test_check_sys(tests)
     use cva, only: IDEAL_EOS, NORMAL_CV_TYPE, CONTINUE_RUN_RC, TIMEOUT_RUN_RC, NEGATIVE_CV_M_TOTAL_RUN_RC, &
                     NEGATIVE_CV_TEMP_RUN_RC, MASS_TOLERANCE_RUN_RC, ENERGY_TOLERANCE_RUN_RC, &
                     MASS_DERIV_TOLERANCE_RUN_RC, ENERGY_DERIV_TOLERANCE_RUN_RC, IDEAL_EOS_RUN_RC, &
-                    MIRROR_X_TOLERANCE_RUN_RC, MIRROR_CV_TYPE, X_LT_X_MIN_RUN_RC, X_GE_X_STOP_RUN_RC, &
+                    MIRROR_X_TOLERANCE_RUN_RC, MIRROR_CV_TYPE, X_LT_X_MIN_RUN_RC, X_GT_X_STOP_RUN_RC, &
                     !X_BLOW_UP_RUN_RC, M_BLOW_UP_RUN_RC, E_BLOW_UP_RUN_RC, E_F_BLOW_UP_RUN_RC, X_DOT_BLOW_UP_RUN_RC, &
                     run_config_type, cv_system_type, run_status_type, check_sys
     
@@ -2122,7 +2122,7 @@ subroutine test_check_sys(tests)
     call check_sys(config, sys, sys_start, t, status)
     call tests%integer_eq(status%rc, CONTINUE_RUN_RC, "test_check_sys, CONTINUE_RUN_RC, status%rc")
     
-    ! `X_GE_X_STOP_RUN_RC`
+    ! `X_GT_X_STOP_RUN_RC`
     
     call sys%cv(1)%x%v%init_const(4.0_WP, N_D)
     call sys%cv(1)%x_dot%v%init_const(0.0_WP, N_D)
@@ -2169,9 +2169,9 @@ subroutine test_check_sys(tests)
     call sys%cv(2)%x_min%v%init_const(0.0_WP, N_D)
     
     call check_sys(config, sys, sys_start, t, status)
-    call tests%integer_eq(status%rc, X_GE_X_STOP_RUN_RC, "test_check_sys, X_GE_X_STOP_RUN_RC, status%rc")
-    call tests%integer_eq(size(status%i_cv), 1, "test_check_sys, X_GE_X_STOP_RUN_RC, size(status%i_cv)")
-    call tests%integer_eq(status%i_cv(1), 1, "test_check_sys, X_GE_X_STOP_RUN_RC, status%i_cv(1)")
+    call tests%integer_eq(status%rc, X_GT_X_STOP_RUN_RC, "test_check_sys, X_GT_X_STOP_RUN_RC, status%rc")
+    call tests%integer_eq(size(status%i_cv), 1, "test_check_sys, X_GT_X_STOP_RUN_RC, size(status%i_cv)")
+    call tests%integer_eq(status%i_cv(1), 1, "test_check_sys, X_GT_X_STOP_RUN_RC, status%i_cv(1)")
     
     ! `TIMEOUT_RUN_RC`
     
@@ -2661,6 +2661,8 @@ subroutine test_check_sys(tests)
     call tests%real_eq(status%data(1), 0.9_WP, "test_check_sys, X_LT_X_MIN_RUN_RC, status%data(1)")
     
     ! TODO: `NEGATIVE_CV_X_RUN_RC`
+    ! TODO: `X_EQ_X_STOP_RUN_RC`
+    ! TODO: `X_EQ_X_MIN_RUN_RC`
     ! TODO: other return codes
 end subroutine test_check_sys
 

test/test_io.f90
@@ -94,7 +94,7 @@ end subroutine test_read_pneumatic_namelist_non_default
 
 subroutine test_read_pneumatic_namelist_default(tests)
     use prec, only: CL, PI
-    use cva, only: DT_DEFAULT, CSV_FREQUENCY_DEFAULT, X_GE_X_STOP_RUN_RC, cv_system_type, run_config_type
+    use cva, only: DT_DEFAULT, CSV_FREQUENCY_DEFAULT, X_GT_X_STOP_RUN_RC, cv_system_type, run_config_type
     use port, only: path_join
     use io, only: I_BARREL, read_pneumatic_namelist
     use gasdata, only: P_ATM, TEMP_ATM
@@ -152,7 +152,7 @@ subroutine test_read_pneumatic_namelist_default(tests)
         call tests%real_eq(sys%cv(I_BARREL_ATM)%p_const%v%v, P_ATM, "read_pneumatic_namelist, default values, p_atm")
         call tests%real_eq(sys%cv(I_BARREL_ATM)%temp_const%v%v, TEMP_ATM, "read_pneumatic_namelist, default values, temp_atm")
         call tests%real_eq(actual_v_muzzle%v%v, 0.0_WP, "read_pneumatic_namelist, default values, actual_v_muzzle")
-        call tests%integer_eq(actual_rc, X_GE_X_STOP_RUN_RC, "read_pneumatic_namelist, default values, actual_rc")
+        call tests%integer_eq(actual_rc, X_GT_X_STOP_RUN_RC, "read_pneumatic_namelist, default values, actual_rc")
     end if
 end subroutine test_read_pneumatic_namelist_default
 
@@ -240,7 +240,7 @@ end subroutine test_read_springer_namelist_non_default
 
 subroutine test_read_springer_namelist_default(tests)
     use prec, only: CL, PI
-    use cva, only: DT_DEFAULT, CSV_FREQUENCY_DEFAULT, X_GE_X_STOP_RUN_RC, cv_system_type, run_config_type
+    use cva, only: DT_DEFAULT, CSV_FREQUENCY_DEFAULT, X_GT_X_STOP_RUN_RC, cv_system_type, run_config_type
     use port, only: path_join
     use io, only: I_BARREL, read_springer_namelist
     use gasdata, only: P_ATM, TEMP_ATM
@@ -315,7 +315,7 @@ subroutine test_read_springer_namelist_default(tests)
         call tests%real_eq(sys%cv(I_BARREL_ATM)%temp_const%v%v, TEMP_ATM, &
                                     "read_springer_namelist, default values, temp_atm (2)")
         call tests%real_eq(actual_v_muzzle%v%v, 00.0_WP, "read_springer_namelist, default values, actual_v_muzzle")
-        call tests%integer_eq(actual_rc, X_GE_X_STOP_RUN_RC, "read_springer_namelist, default values, actual_rc")
+        call tests%integer_eq(actual_rc, X_GT_X_STOP_RUN_RC, "read_springer_namelist, default values, actual_rc")
     end if
 end subroutine test_read_springer_namelist_default
 

commit ba3b674ec109757da1deaad744c4732508eb05d3

fix: A fix for a corner case made previously could lead to stalling, which is now fixed.
Fixes: 56bb154dfff08fa9115f405b748599ccc4fa0b09 ("fix: Addressing corner cases where get_sys_at_x won't be run.")

src/cva.f90
@@ -2004,8 +2004,11 @@ pure subroutine check_sys(config, sys, sys_start, t, status)
         end if
         
         ! Check whether the plunger is at `x_min`, which requires special treatment to get the `l_travel` derivative right.
+        ! The last condition checks whether the plunger was made immobile.
+        ! TODO: Assert that immobile plungers have zero velocity at some point.
         if ((is_close(sys%cv(i_cv)%x%v%v, sys%cv(i_cv)%x_min%v%v)) &
-                .and. (sys%cv(i_cv)%eos /= CONST_EOS)) then
+                .and. (sys%cv(i_cv)%eos /= CONST_EOS) &
+                .and. (.not. is_close(sys%cv(i_cv)%rm_p%v%v, 0.0_WP))) then
             status%rc = X_EQ_X_MIN_RUN_RC
             allocate(status%i_cv(1))
             status%i_cv(1) = i_cv